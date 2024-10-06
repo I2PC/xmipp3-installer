a043e6e90227cfba72c5a91b98881b9b093ad888
Martin Salinas <ssalinasmartin@gmail.com>
2024-10-06 10:59:31 +0200

    Separated formatter code

--- src/xmipp3_installer/application/cli/parsers/mode_help_formatter.py
+++ src/xmipp3_installer/application/cli/parsers/mode_help_formatter.py
@@ -15,50 +15,56 @@ class ModeHelpFormatter(BaseHelpFormatter):
 		"""
 		### This method prints the help message of the argument parser.
 		"""
-		# Getting the selected mode from the parent help message
-		# Message received is the format_help of the main parser's
+		mode = self.__get_mode()
+		help_message = f"{self._get_mode_help(mode, general=False)}\n\n"
+		help_message += self.__get_args_message(mode)
+		help_message += self.__get_examples_message(mode)
+		return format.get_formatting_tabs(help_message)
+
+	def __get_mode(self):
+		"""
+		### Returns the execution mode.
+
+		#### Returns:
+		- (str): Execution mode.
+		"""
+		# Retrieved from the parent help message
+		# Message received is the format_help output of the main parser's
 		# formatter, adding the mode at the end
-		mode = self._prog.split(' ')[-1]
+		return self._prog.split(' ')[-1]
+	
+	def __get_args_message(self, mode: str) -> str:
+		"""
+		### Returns the help section containing all the parameters.
 
-		# Initialize the help message
-		help_message = self._get_mode_help(mode, general=False) + '\n\n'
+		#### Params:
+		- mode (str): Usage mode selected.
 
-		# Get mode args
+		#### Returns:
+		- (str): Help section containing all parameters.
+		"""
 		args = arguments.MODE_ARGS[mode]
-
-		# Add extra messages deppending on if there are args
+		help_message = ''
 		options_str = ''
 		separator = ''
+		
 		if len(args) > 0:
 			arg_names = [self._get_param_first_name(arg_name) for arg_name in args]
-			if self.__argsContainOptional(arg_names):
+			if self.__args_contain_optional(arg_names):
 				help_message += logger.yellow("Note: only params starting with '-' are optional. The rest are required.\n")
 			options_str = ' [options]'
 			separator = self._get_help_separator() + '\t# Options #\n\n'
-		help_message += f'Usage: xmipp {mode}{options_str}\n{separator}'
 
-		# Adding arg info
-		for arg in args:
-			help_message += self._text_with_limits('\t' + ', '.join(getParamNames(arg)), arguments.PARAMS[arg][arguments.DESCRIPTION])
+		help_message += f'Usage: {arguments.XMIPP_PROGRAM_NAME} {mode}{options_str}\n{separator}'
+		help_message += self.__get_args_info(args)
+		return help_message
 
-		# Adding a few examples
-		examples = arguments.MODE_EXAMPLES[mode]
-		for i in range(len(examples)):
-			number_str = '' if len(examples) == 1 else f' {i+1}'	
-			help_message += f"\nExample{number_str}: {examples[i]}"
-		
-		# If any test were added, add extra line break
-		if len(examples) > 0:
-			help_message += '\n'
-
-		return format.get_formatting_tabs(help_message)
-	
-	def __argsContainOptional(self, arg_names: List[str]) -> bool:
+	def __args_contain_optional(self, arg_names: List[str]) -> bool:
 		"""
 		### Returns True if the param name list contains at least one optional param.
 
 		### Params:
-		- arg_names (List[str]): List containing the param names.
+		- arg_names (list[str]): List containing the param names.
 
 		### Returns:
 		- (bool): True if there is at least one optional param. False otherwise.
@@ -67,3 +73,42 @@ class ModeHelpFormatter(BaseHelpFormatter):
 			if name.startswith('-'):
 				return True
 		return False
+
+	def __get_args_info(self, args: List[str]) -> str:
+		"""
+		### Returns the info of each param.
+
+		#### Params:
+		- args (list[str]): List of parameters.
+
+		#### Returns:
+		- (str): Info of all parameters.
+		"""
+		help_message = ''
+		for arg in args:
+			help_message += self._text_with_limits(
+				'\t' + ', '.join(format.get_param_names(arg)),
+				arguments.PARAMS[arg][arguments.DESCRIPTION]
+			)
+		return help_message
+
+	def __get_examples_message(self, mode: str) -> str:
+		"""
+		### Returns the message section containig usage examples.
+
+		#### Params:
+		- mode (str): Usage mode selected.
+
+		#### Returns:
+		- (str): Message section containing usage examples.
+		"""
+		help_message = ''
+		examples = arguments.MODE_EXAMPLES[mode]
+		for i in range(len(examples)):
+			number_str = '' if len(examples) == 1 else f' {i+1}'	
+			help_message += f"\nExample{number_str}: {examples[i]}"
+		
+		if len(examples) > 0:
+			help_message += '\n'
+
+		return help_message
