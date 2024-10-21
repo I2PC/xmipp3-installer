81a08c8df9d4aa4c8805d9a5614cf748d68cf6df
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-10-21 12:22:23 +0200

    Initial variables for integration testing

---
+++ tests/integration/application/cli/cli_test.py
@@ -0,0 +1,6 @@
+from . import terminal_sizes
+
+__TERMINAL_WIDTHS = [
+  terminal_sizes.LARGE_TERMINAL_WIDTH,
+  terminal_sizes.SHORT_TERMINAL_WIDTH
+]
---
+++ tests/integration/application/cli/general_message.py
@@ -0,0 +1,111 @@
+from xmipp3_installer.application.logger.logger import logger
+
+from . import terminal_sizes
+
+GENERAL_HELP_MESSAGE = {
+  terminal_sizes.LARGE_TERMINAL_WIDTH: f"""
+Run Xmipp's installer script
+
+Usage: xmipp [options]
+    ---------------------------------------------
+    # General #
+
+    version [--short]                              Returns the version information. Add '--short' to print only the version number.
+    compileAndInstall [-j] [-b] [--keep-output]    Compiles and installs Xmipp based on already obtained sources.
+    all [-j] [-b] [--keep-output]                  Default param. Runs config, configBuild, and compileAndInstall.
+    configBuild [--keep-output]                    Configures the project with CMake.
+    ---------------------------------------------
+    # Config #
+
+    config [-o]                                    Generates a config file template with default values.
+    ---------------------------------------------
+    # Downloads #
+
+    getModels [-d]                                 Download the DeepLearning Models at dir/models (build by default).
+    getSources [-b] [--keep-output]                Clone all Xmipp's sources.
+    ---------------------------------------------
+    # Clean #
+
+    cleanBin                                       Removes all compiled binaries.
+    cleanAll                                       Removes all compiled binaries and sources, leaves the repository as if freshly cloned (without pulling).       
+    ---------------------------------------------
+    # Test #
+
+    test [testName] [--show]                       Runs a given test.
+    ---------------------------------------------
+    # Developers #
+
+    git [command]                                  Runs the given git action for all source repositories.
+    addModel [login] [modelPath] [--update]        Takes a DeepLearning model from the modelPath, makes a tgz of it and uploads the .tgz according to the <login>.
+
+Example 1: ./xmipp
+Example 2: ./xmipp compileAndInstall -j 4
+{logger.yellow(
+  'Note: You can also view a specific help message for each mode with "./xmipp [mode] -h".\nExample: ./xmipp all -h'
+)}
+  """,
+  terminal_sizes.SHORT_TERMINAL_WIDTH: f"""
+Run Xmipp's installer script
+
+Usage: xmipp [options]
+    ---------------------------------------------
+    # General #
+
+    version [--short]                              Returns the version                                                   information. Add   
+                                                   '--short' to print 
+only
+                                                   the version number.    compileAndInstall [-j] [-b] [--keep-output]    Compiles and installs
+                                                   Xmipp based on already
+                                                   obtained sources.  
+    all [-j] [-b] [--keep-output]                  Default param. Runs                                                   config, configBuild, and
+                                                   compileAndInstall. 
+    configBuild [--keep-output]                    Configures the project
+                                                   with CMake.        
+    ---------------------------------------------
+    # Config #
+
+    config [-o]                                    Generates a config 
+file
+                                                   template with default
+                                                   values.
+    ---------------------------------------------
+    # Downloads #
+
+    getModels [-d]                                 Download the DeepLearning
+                                                   Models at dir/models
+                                                   (build by default).    getSources [-b] [--keep-output]                Clone all Xmipp's  
+                                                   sources.
+    ---------------------------------------------
+    # Clean #
+
+    cleanBin                                       Removes all compiled
+                                                   binaries.
+    cleanAll                                       Removes all compiled
+                                                   binaries and sources,
+                                                   leaves the repository as
+                                                   if freshly cloned  
+                                                   (without pulling). 
+    ---------------------------------------------
+    # Test #
+
+    test [testName] [--show]                       Runs a given test. 
+    ---------------------------------------------
+    # Developers #
+
+    git [command]                                  Runs the given git 
+action
+                                                   for all source     
+                                                   repositories.      
+    addModel [login] [modelPath] [--update]        Takes a DeepLearning
+                                                   model from the modelPath,
+                                                   makes a tgz of it and
+                                                   uploads the .tgz   
+                                                   according to the <login>.
+
+Example 1: ./xmipp
+Example 2: ./xmipp compileAndInstall -j 4
+{logger.yellow(
+  'Note: You can also view a specific help message for each mode with "./xmipp [mode] -h".\nExample: ./xmipp all -h'
+)}
+  """
+}
