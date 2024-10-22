39d0aa333b67841665e909f3df1717503a004353
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-10-22 17:36:17 +0200

    Logger first tests

---
+++ tests/unitary/application/logger/logger_test.py
@@ -0,0 +1,126 @@
+from unittest.mock import patch
+
+import pytest
+
+from xmipp3_installer.application.logger.logger import Logger
+
+from .... import get_assertion_message
+
+def test_formats_green(
+	__mock_color_green,
+	__mock_reset_format
+):
+	original_text = "this is some sample text"
+	logger = Logger()
+	formatted_text = logger.green(original_text)
+	expected_formatted_text = f"{__mock_color_green}{original_text}{__mock_reset_format}"
+	assert (
+		formatted_text == expected_formatted_text
+	), get_assertion_message("green format", expected_formatted_text, formatted_text)
+
+def test_formats_yellow(
+	__mock_color_yellow,
+	__mock_reset_format
+):
+	original_text = "this is some sample text"
+	logger = Logger()
+	formatted_text = logger.yellow(original_text)
+	expected_formatted_text = f"{__mock_color_yellow}{original_text}{__mock_reset_format}"
+	assert (
+		formatted_text == expected_formatted_text
+	), get_assertion_message("green format", expected_formatted_text, formatted_text)
+
+def test_formats_red(
+	__mock_color_red,
+	__mock_reset_format
+):
+	original_text = "this is some sample text"
+	logger = Logger()
+	formatted_text = logger.red(original_text)
+	expected_formatted_text = f"{__mock_color_red}{original_text}{__mock_reset_format}"
+	assert (
+		formatted_text == expected_formatted_text
+	), get_assertion_message("green format", expected_formatted_text, formatted_text)
+
+def test_formats_blue(
+	__mock_color_blue,
+	__mock_reset_format
+):
+	original_text = "this is some sample text"
+	logger = Logger()
+	formatted_text = logger.blue(original_text)
+	expected_formatted_text = f"{__mock_color_blue}{original_text}{__mock_reset_format}"
+	assert (
+		formatted_text == expected_formatted_text
+	), get_assertion_message("green format", expected_formatted_text, formatted_text)
+
+def test_formats_bold(
+	__mock_bold,
+	__mock_reset_format
+):
+	original_text = "this is some sample text"
+	logger = Logger()
+	formatted_text = logger.bold(original_text)
+	expected_formatted_text = f"{__mock_bold}{original_text}{__mock_reset_format}"
+	assert (
+		formatted_text == expected_formatted_text
+	), get_assertion_message("green format", expected_formatted_text, formatted_text)
+
+def test_calls_start_log_file(__mock_open, __mock_singleton):
+	log_file_name = "test_log_file"
+	logger = Logger()
+	logger.start_log_file(log_file_name)
+	__mock_open.assert_called_once_with(log_file_name, 'w')
+
+def test_starts_log_file(__mock_open, __mock_singleton):
+	logger = Logger()
+	logger.start_log_file("test_log_file")
+	assert (
+		logger._Logger__log_file is not None
+	), "Log file did not get properly assigned"
+
+@pytest.fixture
+def __mock_singleton():
+	with patch.object(Logger, "_Logger__instance", None):
+		yield
+
+@pytest.fixture
+def __mock_reset_format():
+	new_format_code = "-format_end"
+	with patch.object(Logger, "_Logger__END_FORMAT", new_format_code):
+		yield new_format_code
+
+@pytest.fixture
+def __mock_color_green():
+	new_format_code = "green_start-"
+	with patch.object(Logger, "_Logger__GREEN", new_format_code):
+		yield new_format_code
+
+@pytest.fixture
+def __mock_color_yellow():
+	new_format_code = "yellow_start-"
+	with patch.object(Logger, "_Logger__YELLOW", new_format_code):
+		yield new_format_code
+
+@pytest.fixture
+def __mock_color_red():
+	new_format_code = "red_start-"
+	with patch.object(Logger, "_Logger__RED", new_format_code):
+		yield new_format_code
+
+@pytest.fixture
+def __mock_color_blue():
+	new_format_code = "blue_start-"
+	with patch.object(Logger, "_Logger__BLUE", new_format_code):
+		yield new_format_code
+
+@pytest.fixture
+def __mock_bold():
+	new_format_code = "bold_start-"
+	with patch.object(Logger, "_Logger__BOLD", new_format_code):
+		yield new_format_code
+
+@pytest.fixture
+def __mock_open():
+	with patch("builtins.open") as mock_method:
+		yield mock_method
