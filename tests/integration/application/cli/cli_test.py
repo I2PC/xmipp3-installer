0fecf2acba8ddcd9d87511bd697bf6645f39d05c
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-10-22 09:14:06 +0200

    Mode cleanAll

--- tests/integration/application/cli/cli_test.py
+++ tests/integration/application/cli/cli_test.py
@@ -12,7 +12,7 @@ from . import general_message
 from .terminal_sizes import LARGE_TERMINAL_WIDTH, SHORT_TERMINAL_WIDTH
 from .mode_messages import (
 	mode_all, mode_version, mode_compile_and_install, mode_config_build, mode_config,
-	mode_get_models, mode_get_sources, mode_clean_bin
+	mode_get_models, mode_get_sources, mode_clean_bin, mode_clean_all
 )
 from .... import get_assertion_message, MockTerminalSize
 
@@ -37,6 +37,8 @@ from .... import get_assertion_message, MockTerminalSize
 		pytest.param(SHORT_TERMINAL_WIDTH, ["getSources"], mode_get_sources.HELP_MESSAGE),
 		pytest.param(LARGE_TERMINAL_WIDTH, ["cleanBin"], mode_clean_bin.HELP_MESSAGE),
 		pytest.param(SHORT_TERMINAL_WIDTH, ["cleanBin"], mode_clean_bin.HELP_MESSAGE),
+		pytest.param(LARGE_TERMINAL_WIDTH, ["cleanAll"], mode_clean_all.HELP_MESSAGE),
+		pytest.param(SHORT_TERMINAL_WIDTH, ["cleanAll"], mode_clean_all.HELP_MESSAGE),
 	],
 	indirect=["__mock_get_terminal_column_size", "__mock_sys_argv"]
 )
