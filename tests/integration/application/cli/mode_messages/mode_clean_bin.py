498cea39049ef43cb140c19338868c60d82da843
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-10-22 08:39:33 +0200

    Mode cleanBin

---
+++ tests/integration/application/cli/mode_messages/mode_clean_bin.py
@@ -0,0 +1,13 @@
+from .. import terminal_sizes
+from . import NOTE_MESSAGE
+
+HELP_MESSAGE = {
+  terminal_sizes.LARGE_TERMINAL_WIDTH: """Removes all compiled binaries.
+
+Usage: xmipp cleanBin
+""",
+  terminal_sizes.SHORT_TERMINAL_WIDTH: """Removes all compiled binaries.
+
+Usage: xmipp cleanBin
+"""
+}
