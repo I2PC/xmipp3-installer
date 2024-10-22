580010fb13fe16223d4f42cf07f65416933f5f49
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-10-22 08:36:45 +0200

    Mode getSources

--- tests/integration/application/cli/cli_test.py
+++ tests/integration/application/cli/cli_test.py
@@ -12,7 +12,7 @@ from . import general_message
 from .terminal_sizes import LARGE_TERMINAL_WIDTH, SHORT_TERMINAL_WIDTH
 from .mode_messages import (
 	mode_all, mode_version, mode_compile_and_install, mode_config_build, mode_config,
-	mode_get_models
+	mode_get_models, mode_get_sources
 )
 from .... import get_assertion_message, MockTerminalSize
 
@@ -33,6 +33,8 @@ from .... import get_assertion_message, MockTerminalSize
 		pytest.param(SHORT_TERMINAL_WIDTH, ["config"], mode_config.HELP_MESSAGE),
 		pytest.param(LARGE_TERMINAL_WIDTH, ["getModels"], mode_get_models.HELP_MESSAGE),
 		pytest.param(SHORT_TERMINAL_WIDTH, ["getModels"], mode_get_models.HELP_MESSAGE),
+		pytest.param(LARGE_TERMINAL_WIDTH, ["getSources"], mode_get_sources.HELP_MESSAGE),
+		pytest.param(SHORT_TERMINAL_WIDTH, ["getSources"], mode_get_sources.HELP_MESSAGE),
 	],
 	indirect=["__mock_get_terminal_column_size", "__mock_sys_argv"]
 )
---
+++ tests/integration/application/cli/mode_messages/mode_get_sources.py
@@ -0,0 +1,32 @@
+from .. import terminal_sizes
+from . import NOTE_MESSAGE
+
+HELP_MESSAGE = {
+  terminal_sizes.LARGE_TERMINAL_WIDTH: f"""Clone all Xmipp's sources.
+
+{NOTE_MESSAGE}Usage: xmipp getSources [options]
+    ---------------------------------------------
+    # Options #
+
+    -b, --branch                                   Branch for the source repositories.
+    --keep-output                                  If set, output sent through the terminal won't substitute lines, looking more like the log.
+
+Example: ./xmipp getSources./xmipp getSources -b devel
+""",
+  terminal_sizes.SHORT_TERMINAL_WIDTH: f"""Clone all Xmipp's sources.
+
+{NOTE_MESSAGE}Usage: xmipp getSources [options]
+    ---------------------------------------------
+    # Options #
+
+    -b, --branch                                   Branch for the source
+                                                   repositories.
+    --keep-output                                  If set, output sent
+                                                   through the terminal
+                                                   won't substitute lines,
+                                                   looking more like the
+                                                   log.
+
+Example: ./xmipp getSources./xmipp getSources -b devel
+"""
+}
