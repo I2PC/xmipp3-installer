b36ed16132f21c0ddda62c9bafb55b0b074d1049
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-10-22 09:14:06 +0200

    Mode cleanAll

---
+++ tests/integration/application/cli/mode_messages/mode_clean_all.py
@@ -0,0 +1,12 @@
+from .. import terminal_sizes
+
+HELP_MESSAGE = {
+  terminal_sizes.LARGE_TERMINAL_WIDTH: """Removes all compiled binaries and sources, leaves the repository as if freshly cloned (without pulling).
+
+Usage: xmipp cleanAll
+""",
+  terminal_sizes.SHORT_TERMINAL_WIDTH: """Removes all compiled binaries and sources, leaves the repository as if freshly cloned (without pulling).
+
+Usage: xmipp cleanAll
+"""
+}
