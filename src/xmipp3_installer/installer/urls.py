d5a58f59541fef5b50621130576e2f187d1574e9
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-10-29 16:50:51 +0100

    Added URL

--- src/xmipp3_installer/installer/urls.py
+++ src/xmipp3_installer/installer/urls.py
@@ -2,3 +2,4 @@
 
 DOCUMENTATION_URL = 'https://i2pc.github.io/docs/'
 CMAKE_INSTALL_DOCS_URL = 'https://i2pc.github.io/docs/Installation/InstallationNotes/index.html#cmake'
+API_URL = 'xmipp.i2pc.es/api/attempts/'
