574a87609fd67ef1e5ea7cfb88ca74acb147a3a3
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-10-29 16:50:29 +0100

    Imported function (WIP)

---
+++ src/xmipp3_installer/api_client/api_client.py
@@ -0,0 +1,27 @@
+import http.client
+import json
+import ssl
+from typing import Dict
+
+from xmipp3_installer.installer import urls
+
+def send_installation_attempt(installation_info: Dict):
+	"""
+	### Sends a POST request to Xmipp's metrics's API.
+	
+	#### Params:
+	- installation_info (dict): Dictionary containing all the installation information.
+	"""
+	if installation_info is not None:
+		params = json.dumps(installation_info)
+		headers = {"Content-type": "application/json"}
+
+		url = urls.API_URL.split("/", maxsplit=1)
+		path = f"/{url[1]}"
+		url = url[0]
+		conn = http.client.HTTPSConnection(
+			url,
+			context=ssl._create_unverified_context() # Unverified context because url does not have an ssl certificate
+		)
+		conn.request("POST", path, params, headers)
+		conn.close()
