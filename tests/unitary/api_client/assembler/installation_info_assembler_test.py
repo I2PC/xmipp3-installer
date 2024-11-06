51e4845c57fe39b867a89990275453b74c3761b9
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-11-06 17:46:11 +0100

    Get log tail tests

--- tests/unitary/api_client/assembler/installation_info_assembler_test.py
+++ tests/unitary/api_client/assembler/installation_info_assembler_test.py
@@ -7,6 +7,8 @@ from xmipp3_installer.installer import constants
 
 from .... import get_assertion_message
 
+__LOG_TAIL = "line1\nline2\nline3\nline4\n"
+
 def test_calls_run_shell_command_when_getting_architecture_name(__mock_run_shell_command):
   installation_info_assembler.__get_architecture_name()
   __mock_run_shell_command.assert_called_once_with('cat /sys/devices/cpu/caps/pmu_name')
@@ -30,12 +32,32 @@ def test_returns_expected_architecture_name(
     architecture_name == expected_architecture_name
   ), get_assertion_message("architecture name", expected_architecture_name, architecture_name)
 
-@pytest.fixture
-def __mock_get_mac_address():
-  with patch(
-    "xmipp3_installer.api_client.assembler.installation_info_assembler.__get_mac_address"
-  ) as mock_method:
-    yield mock_method
+def test_calls_run_shell_command_when_getting_log_tail(__mock_run_shell_command):
+  installation_info_assembler.__get_log_tail()
+  __mock_run_shell_command.assert_called_once_with(
+    f"tail -n {constants.TAIL_LOG_NCHARS} {constants.LOG_FILE}"
+  )
+
+@pytest.mark.parametrize(
+  "ret_code,n_chars,expected_log_tail",
+  [
+    pytest.param(1, 10, None),
+    pytest.param(0, 10, __LOG_TAIL[:10]),
+    pytest.param(0, 0, ""),
+  ],
+)
+def test_returns_expected_log_tail(
+  ret_code,
+  n_chars,
+  expected_log_tail,
+  __mock_run_shell_command
+):
+  mocked_log_tail = ''.join([__LOG_TAIL[letter_index] for letter_index in range(n_chars)])
+  __mock_run_shell_command.return_value = (ret_code, mocked_log_tail)
+  log_tail = installation_info_assembler.__get_log_tail()
+  assert (
+    log_tail == expected_log_tail
+  ), get_assertion_message("log tail", expected_log_tail, log_tail)
 
 @pytest.fixture
 def __mock_run_shell_command(request):
@@ -44,4 +66,11 @@ def __mock_run_shell_command(request):
     "xmipp3_installer.installer.handlers.shell_handler.run_shell_command"
   ) as mock_method:
     mock_method.return_value = (ret_code, output)
+    yield mock_method
+
+@pytest.fixture
+def __mock_get_mac_address():
+  with patch(
+    "xmipp3_installer.api_client.assembler.installation_info_assembler.__get_mac_address"
+  ) as mock_method:
     yield mock_method
