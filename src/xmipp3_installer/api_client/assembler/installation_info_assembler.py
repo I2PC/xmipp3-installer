1a864536e3b9ef2a4f949c841e23cb34e209664f
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-11-08 13:32:46 +0100

    Avoid duplicated function call

--- src/xmipp3_installer/api_client/assembler/installation_info_assembler.py
+++ src/xmipp3_installer/api_client/assembler/installation_info_assembler.py
@@ -120,8 +120,9 @@ def __find_mac_address_in_lines(lines: List[str]) -> Optional[str]:
 	mac_regex = r"link/ether ([0-9a-f:]{17})"
 	interface_regex = r"^\d+: (enp|wlp|eth)\w+"
 	for line in lines:
-		if re.match(interface_regex, line):
-			interface_name = re.match(interface_regex, line).group(1)
+		match = re.match(interface_regex, line)
+		if match:
+			interface_name = match.group(1)
 			if interface_name.startswith(('enp', 'wlp', 'eth')):
 				return re.search(mac_regex, lines[lines.index(line) + 1]).group(1)
 	return None
