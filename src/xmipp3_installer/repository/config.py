a261729cc461feafeb3707f93c6e48164d43495a
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-11-19 18:03:40 +0100

    Add get config date function

--- src/xmipp3_installer/repository/config.py
+++ src/xmipp3_installer/repository/config.py
@@ -1,7 +1,30 @@
 """# Contains functions that interact with the configuration file."""
 
+import re
 import os
+from datetime import datetime
 from typing import List
+
+__LAST_MODIFIED_TEXT = "Config file automatically generated on"
+
+def get_config_date(path: str) -> str:
+  """
+  ### Obtains from the config file the date of its last modification.
+
+  #### Params:
+  - path (str): Path to the config file.
+
+  #### Returns:
+  - (str): Date in dd/mm/yyyy format.
+  """
+  config_lines = __get_file_content(path)
+  for line in config_lines:
+    if __LAST_MODIFIED_TEXT not in line:
+      continue
+    match = re.search(r'\d{4}-\d{2}-\d{2}', line)
+    if match:
+      return datetime.strptime(match.group(), '%Y-%m-%d').strftime('%d/%m/%Y')
+  return ""
 
 def __get_file_content(path: str) -> List[str]:
   """
