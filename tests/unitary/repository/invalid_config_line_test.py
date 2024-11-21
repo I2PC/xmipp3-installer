0fbb97b2a4b03064927599ac93fc164cb93569fe
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-11-21 21:53:57 +0100

    Invalid config line exception tests

---
+++ tests/unitary/repository/invalid_config_line_test.py
@@ -0,0 +1,55 @@
+from unittest.mock import patch
+
+import pytest
+
+from xmipp3_installer.repository.invalid_config_line import InvalidConfigLineError
+
+from ... import get_assertion_message
+
+@pytest.mark.parametrize(
+	"config_file,line_number,line",
+	[
+		pytest.param("test-file", 1, "test line"),
+		pytest.param("other-test-file", 5, "other test line"),
+  ]
+)
+def test_returns_expected_message_when_generating_error_message(
+	config_file,
+	line_number,
+	line,
+	__mock_logger_yellow,
+	__mock_logger_red
+):
+	exception_instance = InvalidConfigLineError()
+	error_message = exception_instance.generate_error_message(
+		config_file,
+		line_number,
+		line
+  )
+	expected_message = '\n'.join([
+    __mock_logger_yellow(f"WARNING: There was an error parsing {config_file} file: "),
+    __mock_logger_red(f'Unable to parse line {line_number}: {line}'),
+    __mock_logger_yellow(
+      "Contents of config file won't be read, default values will be used instead.\n"
+      "You can create a new file template from scratch running './xmipp config -o'."
+    )
+    ])
+	assert (
+		error_message == expected_message
+  ), get_assertion_message("exception error message", expected_message, error_message)
+
+@pytest.fixture
+def __mock_logger_yellow():
+	with patch(
+		"xmipp3_installer.application.logger.logger.Logger.yellow"
+	) as mock_method:
+		mock_method.side_effect = lambda text: f"yellow-start-{text}-yellow-end"
+		yield mock_method
+
+@pytest.fixture
+def __mock_logger_red():
+	with patch(
+		"xmipp3_installer.application.logger.logger.Logger.red"
+	) as mock_method:
+		mock_method.side_effect = lambda text: f"red-start-{text}-red-end"
+		yield mock_method
