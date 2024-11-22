a43b27601afb4f12b31c2777a3570d90b24940b2
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-11-22 09:30:22 +0100

    Separated get conda prefix

---
+++ src/xmipp3_installer/installer/handlers/conda_handler.py
@@ -0,0 +1,11 @@
+import os
+from typing import Optional
+
+def get_conda_prefix_path() -> Optional[str]:
+	"""
+	### Returns the path for the current Conda enviroment.
+
+	#### Returns:
+	- (str | None): Path for current Conda enviroment.
+	"""
+	return os.environ.get('CONDA_PREFIX')
