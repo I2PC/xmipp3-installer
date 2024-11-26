03b6e12c56944c9f6819dca1a5bbcedac8270239
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-11-26 18:50:33 +0100

    Rename

--- tests/integration/repository/config/config_test.py
+++ tests/integration/repository/config/config_test.py
@@ -6,7 +6,7 @@ import pytest
 
 from xmipp3_installer.repository.config import ConfigurationFileHandler
 
-from . import file_contents
+from . import file_content
 from .... import get_assertion_message
 
 def test_writes_default_config_when_there_is_no_config_file(
@@ -15,15 +15,15 @@ def test_writes_default_config_when_there_is_no_config_file(
 ):
 	config_handler = ConfigurationFileHandler(__mock_config_file.name)
 	config_handler.write_config()
-	file_content = ''.join(
+	config_file_content = ''.join(
 		config_handler._ConfigurationFileHandler__get_file_content()
 	).split("\n")
 	assert (
-		file_content == file_contents.DEFAULT_FILE_LINES
+		config_file_content == file_content.DEFAULT_FILE_LINES
 	), get_assertion_message(
 		"config file content",
-		file_contents.DEFAULT_FILE_LINES,
-		file_content
+		file_content.DEFAULT_FILE_LINES,
+		config_file_content
 	)
 
 @pytest.fixture
@@ -39,6 +39,6 @@ def __mock_config_file():
 def __mock_datetime_strftime():
 	with patch("xmipp3_installer.repository.config.datetime") as mock_lib:
 		mock_today = Mock()
-		mock_today.strftime.return_value = file_contents.DATE
+		mock_today.strftime.return_value = file_content.DATE
 		mock_lib.today.return_value = mock_today
 		yield mock_lib
