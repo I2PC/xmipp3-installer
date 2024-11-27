0ddd1a212960bbf10ec0cab8ce8f36dbcf11d176
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-11-27 11:10:54 +0100

    Modified variable test

--- tests/integration/repository/config/config_test.py
+++ tests/integration/repository/config/config_test.py
@@ -9,6 +9,8 @@ from xmipp3_installer.repository.config import ConfigurationFileHandler
 from . import file_content
 from .... import get_assertion_message
 
+__ASSERTION_CONTEXT = "config file content"
+
 def test_writes_default_config_when_there_is_no_config_file(
 	__mock_config_file,
 	__mock_datetime_strftime
@@ -21,7 +23,7 @@ def test_writes_default_config_when_there_is_no_config_file(
 	assert (
 		config_file_content == file_content.DEFAULT_FILE_LINES
 	), get_assertion_message(
-		"config file content",
+		__ASSERTION_CONTEXT,
 		file_content.DEFAULT_FILE_LINES,
 		config_file_content
 	)
@@ -30,8 +32,10 @@ def test_writes_unkown_variables_to_config_when_unkown_variables_are_added_to_va
 	__mock_config_file,
 	__mock_datetime_strftime
 ):
+	unknown_key = "UNKNOWN"
+	unknown_value = "myvalue"
 	config_handler = ConfigurationFileHandler(__mock_config_file.name)
-	config_handler.values.update({"UNKNOWN": "myvalue"})
+	config_handler.values.update({unknown_key: unknown_value})
 	config_handler.write_config()
 	config_file_content = ''.join(
 		config_handler._ConfigurationFileHandler__get_file_content()
@@ -39,14 +43,37 @@ def test_writes_unkown_variables_to_config_when_unkown_variables_are_added_to_va
 	expected_file_content = [
 		*file_content.MANDATORY_SECTIONS_LINES,
 		*file_content.UNKNOWN_VARIABLES_HEADER,
-		"UNKNOWN=myvalue",
+		f"{unknown_key}={unknown_value}",
 		"",
 		*file_content.LAST_MODIFIED_LINES
 	]
 	assert (
 		config_file_content == expected_file_content
 	), get_assertion_message(
-		"config file content",
+		__ASSERTION_CONTEXT,
+		expected_file_content,
+		config_file_content
+	)
+
+def test_writes_modified_variables_to_config_when_some_variable_values_are_changed(
+	__mock_config_file,
+	__mock_datetime_strftime
+):
+	modified_key = "SEND_INSTALLATION_STATISTICS"
+	modified_value = "OFF"
+	config_handler = ConfigurationFileHandler(__mock_config_file.name)
+	config_handler.values.update({modified_key: modified_value})
+	config_handler.write_config()
+	config_file_content = ''.join(
+		config_handler._ConfigurationFileHandler__get_file_content()
+	).split("\n")
+	expected_file_content = '\n'.join(
+		file_content.DEFAULT_FILE_LINES
+	).replace(f"{modified_key}=ON", f"{modified_key}={modified_value}").split("\n")
+	assert (
+		config_file_content == expected_file_content
+	), get_assertion_message(
+		__ASSERTION_CONTEXT,
 		expected_file_content,
 		config_file_content
 	)
