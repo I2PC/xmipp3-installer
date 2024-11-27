78c0791aa13ec9e6fe82614c54812ce50e2c4137
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-11-27 09:34:46 +0100

    Unkown variables test

--- tests/integration/repository/config/config_test.py
+++ tests/integration/repository/config/config_test.py
@@ -26,6 +26,33 @@ def test_writes_default_config_when_there_is_no_config_file(
 		config_file_content
 	)
 
+def test_writes_unkown_variables_to_config_when_unkown_variables_are_added_to_values(
+	__mock_config_file,
+	__mock_datetime_strftime
+):
+	config_handler = ConfigurationFileHandler(__mock_config_file.name)
+	config_handler.values.update({"UNKNOWN": "myvalue"})
+	config_handler.write_config()
+	config_file_content = ''.join(
+		config_handler._ConfigurationFileHandler__get_file_content()
+	).split("\n")
+	expected_file_content = [
+		*file_content.TOGGLE_SECTION_LINES,
+		*file_content.PACKAGE_HOME_SECTION_LINES,
+		*file_content.COMPILATION_FLAGS_SECTION_LINES,
+		*file_content.UNKNOWN_VARIABLES_HEADER,
+		"UNKNOWN=myvalue",
+		"",
+		*file_content.LAST_MODIFIED_LINES
+	]
+	assert (
+		config_file_content == expected_file_content
+	), get_assertion_message(
+		"config file content",
+		expected_file_content,
+		config_file_content
+	)
+
 @pytest.fixture
 def __mock_config_file():
 	with tempfile.NamedTemporaryFile(
