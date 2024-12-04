1b7bb18999cc41503ee48adee11ca4172eb1c127
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-12-04 17:55:32 +0100

    Awaited response with timeout

--- src/xmipp3_installer/api_client/api_client.py
+++ src/xmipp3_installer/api_client/api_client.py
@@ -14,23 +14,29 @@ def send_installation_attempt(installation_info: Dict):
 	#### Params:
 	- installation_info (dict): Dictionary containing all the installation information.
 	"""
-	if installation_info is not None:
-		params = json.dumps(installation_info)
-		headers = {"Content-type": "application/json"}
-		parsed_url = urlparse(urls.API_URL)
-		conn = __get_https_connection(parsed_url)
+	if installation_info is None:
+		return
+	params = json.dumps(installation_info)
+	headers = {"Content-type": "application/json"}
+	parsed_url = urlparse(urls.API_URL)
+	conn = __get_https_connection(parsed_url, 2)
+	try:
 		conn.request("POST", parsed_url.path, body=params, headers=headers)
-		conn.close()
+		conn.getresponse()
+	except TimeoutError:
+		pass
+	conn.close()
 
-def __get_https_connection(parsed_url: ParseResult) -> http.client.HTTPSConnection:
+def __get_https_connection(parsed_url: ParseResult, timeout_seconds: int) -> http.client.HTTPSConnection:
 	"""
 	### Establishes the connection needed to send the API call.
 	Separated to enable integration & E2E tests.
 
 	#### Params:
 	- parsed_url (ParseResult): Object containing all elements of the url.
+	- timeout_seconds (int): Number of seconds to wait until a timeout.
 
 	#### Returns:
 	- (HTTPSConnection): Established connection.
 	"""
-	return http.client.HTTPSConnection(parsed_url.hostname, parsed_url.port)
+	return http.client.HTTPSConnection(parsed_url.hostname, parsed_url.port, timeout=timeout_seconds)
