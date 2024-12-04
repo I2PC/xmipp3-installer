27bec2dc96efffa1ef92e94e9b3e38b2ef15379e
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-12-04 17:15:01 +0100

    Separated connection establishment for testing

--- src/xmipp3_installer/api_client/api_client.py
+++ src/xmipp3_installer/api_client/api_client.py
@@ -3,7 +3,7 @@
 import http.client
 import json
 from typing import Dict
-from urllib.parse import urlparse
+from urllib.parse import urlparse, ParseResult
 
 from xmipp3_installer.installer import urls
 
@@ -18,6 +18,19 @@ def send_installation_attempt(installation_info: Dict):
 		params = json.dumps(installation_info)
 		headers = {"Content-type": "application/json"}
 		parsed_url = urlparse(urls.API_URL)
-		conn = http.client.HTTPConnection(parsed_url.hostname, parsed_url.port)
+		conn = __get_https_connection(parsed_url)
 		conn.request("POST", parsed_url.path, body=params, headers=headers)
 		conn.close()
+
+def __get_https_connection(parsed_url: ParseResult) -> http.client.HTTPSConnection:
+	"""
+	### Establishes the connection needed to send the API call.
+	Separated to enable integration & E2E tests.
+
+	#### Params:
+	- parsed_url (ParseResult): Object containing all elements of the url.
+
+	#### Returns:
+	- (HTTPSConnection): Established connection.
+	"""
+	return http.client.HTTPSConnection(parsed_url.hostname, parsed_url.port)
