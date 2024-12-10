4adb1bb8c4aa90204ddfde174be6e82c477c0cfe
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-12-10 18:11:40 +0100

    First approach at mode execution

--- src/xmipp3_installer/installer/installer_service.py
+++ src/xmipp3_installer/installer/installer_service.py
@@ -1,4 +1,10 @@
+import sys
 from typing import Dict
 
+from xmipp3_installer.application.cli.arguments import modes
+from xmipp3_installer.installer.modes import mode_selector
+
 def run_installer(args: Dict):
-  pass
+  mode = args.get(modes.MODE, modes.MODE_ALL)
+  ret_code = bool(mode_selector.MODE_FUNCTIONS[mode](args)) # Ret code?
+  sys.exit(ret_code)
