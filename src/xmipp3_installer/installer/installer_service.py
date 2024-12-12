0bcb2683895923ec5be5f409ce318d5cc7b05f21
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-12-12 14:30:55 +0100

    Changed installer logic to use executor interface

--- src/xmipp3_installer/installer/installer_service.py
+++ src/xmipp3_installer/installer/installer_service.py
@@ -6,5 +6,5 @@ from xmipp3_installer.installer.modes import mode_selector
 
 def run_installer(args: Dict):
   mode = args.get(modes.MODE, modes.MODE_ALL)
-  ret_code = bool(mode_selector.MODE_FUNCTIONS[mode](args)) # Ret code?
-  sys.exit(ret_code)
+  mode_executor = mode_selector.MODE_EXECUTORS[mode](args)
+  sys.exit(mode_executor.run())
--- src/xmipp3_installer/installer/modes/mode_selector.py
+++ src/xmipp3_installer/installer/modes/mode_selector.py
@@ -1,14 +1,14 @@
 from xmipp3_installer.application.cli.arguments import modes
-from xmipp3_installer.installer.modes import mode_config
+from xmipp3_installer.installer.modes import mode_config_executor
 
-MODE_FUNCTIONS = {
+MODE_EXECUTORS = {
   modes.MODE_ADD_MODEL: NotImplemented,
   modes.MODE_ALL: NotImplemented,
   modes.MODE_CLEAN_ALL: NotImplemented,
   modes.MODE_CLEAN_BIN: NotImplemented,
   modes.MODE_COMPILE_AND_INSTALL: NotImplemented,
   modes.MODE_CONFIG_BUILD: NotImplemented,
-  modes.MODE_CONFIG: mode_config.mode_config,
+  modes.MODE_CONFIG: mode_config_executor.ModeConfigExecutor,
   modes.MODE_GET_MODELS: NotImplemented,
   modes.MODE_GET_SOURCES: NotImplemented,
   modes.MODE_GIT: NotImplemented,
