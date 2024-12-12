129abc48b9b3ac8bd44aa78a71e2f45b89153ccc
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-12-12 15:50:50 +0100

    Log error on executor failure

--- src/xmipp3_installer/installer/installer_service.py
+++ src/xmipp3_installer/installer/installer_service.py
@@ -3,8 +3,13 @@ from typing import Dict
 
 from xmipp3_installer.application.cli.arguments import modes
 from xmipp3_installer.installer.modes import mode_selector
+from xmipp3_installer.application.logger.logger import logger
 
 def run_installer(args: Dict):
   mode = args.get(modes.MODE, modes.MODE_ALL)
   mode_executor = mode_selector.MODE_EXECUTORS[mode](args)
+  ret_code, output = mode_executor.run()
+  if ret_code:
+    logger.log_error(output, ret_code=ret_code)
+  # Else print done?
   sys.exit(mode_executor.run())
