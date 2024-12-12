b9f711cc670c0a8efff0fac835ed1be0c282fce7
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-12-12 15:49:57 +0100

    Refine functionality

--- src/xmipp3_installer/installer/modes/mode_config_executor.py
+++ src/xmipp3_installer/installer/modes/mode_config_executor.py
@@ -1,6 +1,7 @@
-from typing import Dict
+from typing import Dict, Tuple
 
 from xmipp3_installer.installer.modes import mode_executor
+from xmipp3_installer.application.logger import errors
 from xmipp3_installer.application.cli.arguments import params
 from xmipp3_installer.repository import config
 
@@ -8,13 +9,20 @@ class ModeConfigExecutor(mode_executor.ModeExecutor):
 	def __init__(self, args: Dict):
 		super().__init__(args)
 		self.overwrite = args.get(params.PARAM_OVERWRITE, False)
+		self.config_values = {}
 	
-	def run(self) -> int:
+	def run(self) -> Tuple[int, str]:
+		"""
+		### Reads the config file and writes to it formatting properly with the appropiate values.
+
+		#### Returns:
+		- (tuple(int, str)): Tuple containing the error status and an error message if there was an error. 
+		"""
 		try:
 			file_handler = config.ConfigurationFileHandler()
 			file_handler.write_config(overwrite=self.overwrite)
-		except PermissionError:
-			return 1
+		except PermissionError as permission_error:
+			return errors.IO_ERROR, str(permission_error)
 		self.config_values = file_handler.values
-		return 0
+		return 0, ""
 	
