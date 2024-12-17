522450ee0876cdf0c8cf8891435d1ed25df68679
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-12-17 15:36:37 +0100

    Separated test executions by test type

--- scripts/run-tests.ps1
+++ scripts/run-tests.ps1
@@ -1,12 +1,61 @@
+param (
+  [string]$TestType = "all"
+)
+
 if ($env:DEBUG -eq 'true') {
-  Set-PSDebug -Trace 1
+  $DebugPreference = "Continue"
 }
 
-$ROOT_DIR = Split-Path $PSScriptRoot -Parent
-$LAST_LOCATION = Get-Location
-Set-Location $ROOT_DIR
+$CURRENT_DIR = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$ROOT_DIR = Join-Path $CURRENT_DIR ".."
 
-$RCFILE="$ROOT_DIR\conf\.coveragerc"
-python -m pytest -v --cache-clear --cov --cov-config=$RCFILE --junitxml=report.xml --cov-report xml --cov-report term
+$RCFOLDER = Join-Path $CURRENT_DIR "../conf/coverage"
+$RCFILE_UNITARY = Join-Path $RCFOLDER ".unitary-coveragerc"
+$RCFILE_INTEGRATION = Join-Path $RCFOLDER ".integration-coveragerc"
+$RCFILE_E2E = Join-Path $RCFOLDER ".e2e-coveragerc"
 
-Set-Location $LAST_LOCATION
+$CONFIGFOLDER = Join-Path $CURRENT_DIR "../conf/pytest"
+$CONFIGFILE_UNITARY = Join-Path $CONFIGFOLDER "unitary.ini"
+$CONFIGFILE_INTEGRATION = Join-Path $CONFIGFOLDER "integration.ini"
+$CONFIGFILE_E2E = Join-Path $CONFIGFOLDER "e2e.ini"
+
+function Run-Tests {
+  param (
+    [string]$TestType,
+    [string]$RcFile,
+    [string]$ConfFile
+  )
+
+  Write-Host "Running $TestType tests..."
+  $pytestCommand = "python -m pytest -v --cache-clear --cov --cov-config=$RcFile -c=$ConfFile --rootdir=$ROOT_DIR --junitxml=report.xml --cov-report xml --cov-report term"
+  Invoke-Expression $pytestCommand
+  if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+  }
+}
+
+Push-Location $ROOT_DIR
+
+  switch ($TestType) {
+    "unitary" {
+      Run-Tests "unitary" $RCFILE_UNITARY $CONFIGFILE_UNITARY
+    }
+    "integration" {
+      Run-Tests "integration" $RCFILE_INTEGRATION $CONFIGFILE_INTEGRATION
+    }
+    "e2e" {
+      Run-Tests "e2e" $RCFILE_E2E $CONFIGFILE_E2E
+    }
+    "all" {
+      Run-Tests "unitary" $RCFILE_UNITARY $CONFIGFILE_UNITARY
+      Run-Tests "integration" $RCFILE_INTEGRATION $CONFIGFILE_INTEGRATION
+      Run-Tests "e2e" $RCFILE_E2E $CONFIGFILE_E2E
+    }
+    default {
+      Write-Host "Invalid test type: $TestType"
+      Write-Host "Valid types:`n`tall (default)`n`tunitary`n`tintegration`n`te2e"
+      exit 1
+    }
+  }
+
+Pop-Location
--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,15 +3,55 @@
 [[ "${DEBUG}" == 'true' ]] && set -o xtrace
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
+ROOT_DIR="$CURRENT_DIR/.."
 
-RCFILE=$CURRENT_DIR/../conf/.coveragerc
+RCFOLDER=$CURRENT_DIR/../conf/coverage
+RCFILE_UNITARY=$RCFOLDER/.unitary-coveragerc
+RCFILE_INTEGRATION=$RCFOLDER/.integration-coveragerc
+RCFILE_E2E=$RCFOLDER/.e2e-coveragerc
 
-pushd "${CURRENT_DIR}"/.. > /dev/null
+CONFIGFOLDER=$CURRENT_DIR/../conf/pytest
+CONFIGFILE_UNITARY=$CONFIGFOLDER/unitary.ini
+CONFIGFILE_INTEGRATION=$CONFIGFOLDER/integration.ini
+CONFIGFILE_E2E=$CONFIGFOLDER/e2e.ini
 
-    python -m pytest -v --cache-clear --cov --cov-config=$RCFILE --junitxml=report.xml --cov-report xml --cov-report term
+run_tests() {
+    local test_type=$1
+    local rcfile=$2
+    local conffile=$3
+
+    echo "Running ${test_type} tests..."
+    python -m pytest -v --cache-clear --cov --cov-config=$rcfile -c=$conffile --rootdir="${ROOT_DIR}" --junitxml=report.xml --cov-report xml --cov-report term
     PYTEST_EXIT_CODE=$?
     if [ $PYTEST_EXIT_CODE -ne 0 ]; then
         exit $PYTEST_EXIT_CODE
     fi
+}
+
+pushd "${ROOT_DIR}" > /dev/null
+
+    TEST_TYPE=${1:-all}
+
+    case $TEST_TYPE in
+        unitary)
+            run_tests "unitary" $RCFILE_UNITARY $CONFIGFILE_UNITARY
+            ;;
+        integration)
+            run_tests "integration" $RCFILE_INTEGRATION $CONFIGFILE_INTEGRATION
+            ;;
+        e2e)
+            run_tests "e2e" $RCFILE_E2E $CONFIGFILE_E2E
+            ;;
+        all)
+            run_tests "unitary" $RCFILE_UNITARY $CONFIGFILE_UNITARY
+            run_tests "integration" $RCFILE_INTEGRATION $CONFIGFILE_INTEGRATION
+            run_tests "e2e" $RCFILE_E2E $CONFIGFILE_E2E
+            ;;
+        *)
+            echo "Invalid test type: $TEST_TYPE"
+            echo -e "Valid types:\n\tall (default)\n\tunitary\n\tintegration\n\te2e"
+            exit 1
+            ;;
+    esac
 
 popd > /dev/null
