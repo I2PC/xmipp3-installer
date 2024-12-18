a7fe8294d9f39ee78f91ce8c596b3a7167dea1f3
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-12-18 08:57:00 +0100

    Add extra garbage files to clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,3 +5,6 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
 
 find $CURRENT_DIR/../ | grep -E "(/__pycache__$|\.pyc$|\.pyo$)" | xargs rm -rf
+find $CURRENT_DIR/../ -name ".coverage.*" | xargs rm -f
+find $CURRENT_DIR/../ -name "coverage.xml" | xargs rm -f
+find $CURRENT_DIR/../ -name "report.xml" | xargs rm -f
