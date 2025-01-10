7d98882fe752b593ea9655e78548274236965775
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-01-10 19:06:35 +0100

    E2E test for full message

--- tests/e2e/mode_version_test.py
+++ tests/e2e/mode_version_test.py
@@ -1,8 +1,22 @@
 import subprocess
 
+from xmipp3_installer.api_client.assembler import installation_info_assembler
+from xmipp3_installer.application.logger.logger import logger
 from xmipp3_installer.installer.tmp import versions
 
 from .. import get_assertion_message
+
+__TITLE = f"Xmipp {versions.XMIPP_VERSIONS[versions.XMIPP][versions.VERSION_KEY]} (installer-logic)"
+__SOURCE_NOT_FOUND_MESSAGE = logger.yellow("Not found")
+__FULL_INFO_BEFORE_CONFIG = f"""{logger.bold(__TITLE)}
+
+Release date:            {versions.RELEASE_DATE}
+Compilation date:        -
+System version:          {installation_info_assembler.get_os_release_name()}
+xmippCore branch:        {__SOURCE_NOT_FOUND_MESSAGE}
+xmippViz branch:         {__SOURCE_NOT_FOUND_MESSAGE}
+scipion-em-xmipp branch: {__SOURCE_NOT_FOUND_MESSAGE}
+"""
 
 def test_returns_short_version():
 	command_words = ["xmipp3_installer", "version", "--short"]
@@ -11,3 +25,10 @@ def test_returns_short_version():
 	assert (
 		result.stdout == expected_version
 	), get_assertion_message("short version", expected_version, result.stdout)
+
+def test_returns_full_version_before_config():
+	command_words = ["xmipp3_installer", "version"]
+	result = subprocess.run(command_words, capture_output=True, text=True)
+	assert (
+		result.stdout == __FULL_INFO_BEFORE_CONFIG
+	), get_assertion_message("full version", __FULL_INFO_BEFORE_CONFIG, result.stdout)
