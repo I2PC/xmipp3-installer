dda7100e889386ee1650412089814d8fd091389f
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-01-11 17:34:03 +0100

    Get library versions section

--- src/xmipp3_installer/installer/modes/mode_version_executor.py
+++ src/xmipp3_installer/installer/modes/mode_version_executor.py
@@ -1,13 +1,14 @@
 import os
 from typing import Dict, Tuple
 
+from xmipp3_installer.application.cli.arguments import params
+from xmipp3_installer.application.logger.logger import logger
+from xmipp3_installer.api_client.assembler import installation_info_assembler
+from xmipp3_installer.installer import constants
 from xmipp3_installer.installer.modes import mode_executor
 from xmipp3_installer.installer.tmp import versions
 from xmipp3_installer.installer.handlers import git_handler
-from xmipp3_installer.installer import constants
-from xmipp3_installer.application.logger.logger import logger
-from xmipp3_installer.application.cli.arguments import params
-from xmipp3_installer.api_client.assembler import installation_info_assembler
+from xmipp3_installer.installer.handlers.cmake import cmake_handler
 from xmipp3_installer.repository import config
 
 class ModeVersionExecutor(mode_executor.ModeExecutor):
@@ -37,6 +38,7 @@ class ModeVersionExecutor(mode_executor.ModeExecutor):
 			system_version_left_text = self.__add_padding_spaces("System version: ")
 			logger(f"{system_version_left_text}{installation_info_assembler.get_os_release_name()}")
 			logger(self.__get_sources_info())
+			logger(f"\n{self.__get_library_versions_section()}")
 		return 0, ""
 
 	def __get_dates_section(self) -> str:
@@ -115,3 +117,19 @@ class ModeVersionExecutor(mode_executor.ModeExecutor):
 			return left_text
 		spaces = ''.join([' ' for _ in range(self.__LEFT_TEXT_LEN - text_len)])
 		return f"{left_text}{spaces}"
+
+	def __get_library_versions_section(self) -> str:
+		"""
+		### Retrieves the version of the libraries used in the project.
+		
+		#### Returns:
+		- (str): Libraries with their version.
+		"""
+		if not os.path.exists(constants.LIBRARY_VERSIONS_FILE):
+			return ""
+		version_lines = [] 
+		versions = cmake_handler.get_library_versions_from_cmake_file(constants.LIBRARY_VERSIONS_FILE)
+		for library, version in versions.items():
+			library_left_text = self.__add_padding_spaces(f"{library}: ")
+			version_lines.append(f"{library_left_text}{version}")
+		return '\n'.join(version_lines)
