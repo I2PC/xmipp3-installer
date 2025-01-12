c4c62a425e65b5a3fd76fe692a6698cb774843c4
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-01-12 17:42:09 +0100

    Obtain values only in runtime

--- tests/e2e/shell_command_outputs/mode_version.py
+++ tests/e2e/shell_command_outputs/mode_version.py
@@ -4,6 +4,11 @@ from xmipp3_installer.installer.tmp import versions
 from xmipp3_installer.installer.handlers import git_handler
 from xmipp3_installer.installer.modes.mode_version_executor import ModeVersionExecutor
 
+def __get_found_source_message():
+  version_executor = ModeVersionExecutor({})
+  source_message_line:str = version_executor._ModeVersionExecutor__get_source_info("")
+  return source_message_line.replace("branch:", "").strip()
+
 __TITLE = f"Xmipp {versions.XMIPP_VERSIONS[versions.XMIPP][versions.VERSION_KEY]} ({git_handler.get_current_branch()})"
 
 __SOURCE_NOT_FOUND_MESSAGE = logger.yellow("Not found")
@@ -19,8 +24,7 @@ MPI:                     3.1
 HDF5:                    1.10.10
 JPEG:                    80
 SQLite3:                 3.45.1
-Java:                    17.0.13
-"""
+Java:                    17.0.13"""
 
 __COMMON_SECTION = f"""{logger.bold(__TITLE)}
 
@@ -32,15 +36,39 @@ __SOURCES_NOT_FOUND_SECTION = f"""xmippCore branch:        {__SOURCE_NOT_FOUND_M
 xmippViz branch:         {__SOURCE_NOT_FOUND_MESSAGE}
 scipion-em-xmipp branch: {__SOURCE_NOT_FOUND_MESSAGE}"""
 
-FULL_INFO_BEFORE_CONFIG = f"""{__COMMON_SECTION}
-{__SOURCES_NOT_FOUND_SECTION}
+def get_sources_found_section():
+  found_source_message = __get_found_source_message()
+  return '\n'.join([
+    f"xmippCore branch:        {found_source_message}",
+    f"xmippViz branch:         {found_source_message}",
+    f"scipion-em-xmipp branch: {found_source_message}"
+  ])
 
-{__WARNING_MESSAGE}
-"""
+def get_full_info_before_config():
+  return '\n'.join([
+    __COMMON_SECTION,
+    __SOURCES_NOT_FOUND_SECTION,
+    "",
+    __WARNING_MESSAGE,
+    ""
+  ])
 
-FULL_INFO_AFTER_CONFIG_NO_SOURCES = f"""{__COMMON_SECTION}
-{__SOURCES_NOT_FOUND_SECTION}
+def get_full_info_after_config_without_sources():
+  return '\n'.join([
+    __COMMON_SECTION,
+    __SOURCES_NOT_FOUND_SECTION,
+    "",
+    __LIBRARIES_WITH_VERSIONS,
+    "",
+    __WARNING_MESSAGE,
+    ""
+  ])
 
-{__LIBRARIES_WITH_VERSIONS}
-{__WARNING_MESSAGE}
-"""
+def get_full_info_after_config_with_sources():
+  return '\n'.join([
+    __COMMON_SECTION,
+    get_sources_found_section(),
+    "",
+    __LIBRARIES_WITH_VERSIONS,
+    ""
+  ])
