15d3ec7e2d79f24606c641b9f3b58e22cf989563
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-01-13 17:06:12 +0100

    Removed old code

--- original/installer/main.py
+++ original/installer/main.py
@@ -25,20 +25,8 @@
 This module contains the necessary functions to run most installer commands.
 """
 
-# General imports
 import os, sys
 from typing import Tuple, Optional
-
-# Module imports
-from .utils import runJob, getCurrentBranch, isProductionMode
-from .logger import logger, yellow, green, bold
-from .constants import (REPOSITORIES, XMIPP_SOURCES, SOURCES_PATH, MASTER_BRANCHNAME,
-	SOURCE_CLONE_ERROR, TAG_BRANCH_NAME, INTERRUPTED_ERROR, VERSION_FILE, RELEASE_DATE,
-	XMIPP_VERSIONS, XMIPP, VERSION_KEY, SECTION_MESSAGE_LEN, VERNAME_KEY, MODE_GET_SOURCES,
-	MODE_CONFIG_BUILD, CONFIG_FILE)
-from .api import sendApiPOST, getOSReleaseName
-from .cmake import parseCmakeVersions
-from .config import getConfigDate
 
 ####################### COMMAND FUNCTIONS #######################
 def getSources(branch: str=None):
@@ -91,100 +79,6 @@ def handleRetCode(realRetCode: int, predefinedErrorCode: int=0, message: str='')
 	__logDoneMessage()
 	logger("", forceConsoleOutput=True)
 
-def getSuccessMessage() -> str:
-	"""
-	### This function returns the message shown when Xmipp is compiled successfully.
-	
-	#### Returms:
-	- (str): Success message.
-	"""
-	# Getting release name
-	branchName = getCurrentBranch()
-	releaseName = branchName
-	if branchName is None or branchName == MASTER_BRANCHNAME:
-		releaseName = XMIPP_VERSIONS[XMIPP][VERSION_KEY]
-
-	# Creating message line
-	releaseMessage = 'Xmipp {} has been successfully installed, enjoy it!'.format(releaseName)
-	releaseMessageWrapper = '*  *'
-	messageLine = releaseMessageWrapper[:int(len(releaseMessageWrapper)/2)]
-	messageLine += green(f'Xmipp {releaseName} has been successfully installed, enjoy it!')
-	messageLine += releaseMessageWrapper[int(len(releaseMessageWrapper)/2):]
-
-	# Creating box around message line
-	totalLen = len(releaseMessage) + len(releaseMessageWrapper)
-	topBottomBorder = ''.join(['*' for _ in range(totalLen)])
-	marginLine = f"*{''.join([' ' for _ in range(totalLen - 2)])}*"
-
-	return '\n'.join([topBottomBorder, marginLine, messageLine, marginLine, topBottomBorder])
-
-def getSectionMessage(text: str) -> str:
-	"""
-	### This function prints a section message in a specific format.
-
-	#### Params:
-	- text (str): Title of the section.
-
-	#### Returns:
-	- (str): Formatted section message.
-	"""
-	# Check if provided text's length has exceeded specified limit
-	textLen = len(text)
-	remainingLen = SECTION_MESSAGE_LEN - textLen
-	if remainingLen < 4:
-		return text
-	
-	# Calculating characters around given text
-	nDashes = remainingLen - 2
-	nFinalDashes = int(nDashes / 2)
-	nInitialDashes = nDashes - nFinalDashes
-	finalDashes = ''.join(['-' for _ in range(nFinalDashes)])
-	initialDashes = ''.join(['-' for _ in range(nInitialDashes)])
-	return f"{initialDashes} {text} {finalDashes}"
-
-def getVersionMessage(short: bool=False) -> str:
-	"""
-	### This function returns the message for the version mode.
-
-	#### Params:
-	- short (bool): Optional. If True, only Xmipp's version with name will be returned.
-
-	#### Returns:
-	- (str): Message for version mode.
-	"""
-	if short:
-		return XMIPP_VERSIONS[XMIPP][VERNAME_KEY]
-	
-	# Main info
-	rightSectionStart = 25
-	versionType = 'release' if isProductionMode() else getCurrentBranch()
-	versionMessage = bold(f"Xmipp {XMIPP_VERSIONS[XMIPP][VERSION_KEY]} ({versionType})\n\n")
-	versionMessage += f"{__getVersionLineWithSpaces('Release date: ', rightSectionStart)}{RELEASE_DATE}\n"
-	versionMessage += f"{__getVersionLineWithSpaces('Compilation date: ', rightSectionStart)}{getConfigDate(CONFIG_FILE)}\n"
-	versionMessage += f"{__getVersionLineWithSpaces('System version: ', rightSectionStart)}{getOSReleaseName()}\n"
-
-	# Get sources branch
-	for source in XMIPP_SOURCES:
-		sourcePath = os.path.join(SOURCES_PATH, source)
-		currentCommit = __getCurrentCommit(dir=sourcePath)
-		branchName = getCurrentBranch(dir=sourcePath)
-		branchName = branchName if branchName != 'HEAD' else __getCommitName(currentCommit, dir=sourcePath)
-		sourceText = f"{source} branch: "
-		versionMessage += f"{__getVersionLineWithSpaces(sourceText, rightSectionStart)}{branchName} ({currentCommit})\n"
-	versionMessage += "\n"
-
-	# Getting versions from version file
-	if os.path.exists(VERSION_FILE):
-		versionDict = parseCmakeVersions(VERSION_FILE)
-		for package in versionDict:
-			versionMessage += f"{__getVersionLineWithSpaces(f'{package}: ', rightSectionStart)}{versionDict[package]}\n"
-		versionMessage = versionMessage[:-1]
-	else:
-		warningStr = "This project has not yet been configured, so some detectable dependencies have not been properly detected.\n"
-		warningStr += f"Run '{MODE_GET_SOURCES}' and then '{MODE_CONFIG_BUILD}' to be able to show all detectable."
-		versionMessage += yellow(warningStr)
-	return versionMessage
-
 ####################### AUX FUNCTIONS #######################
 def __branchExists(repo: str, branch: str) -> bool:
 	"""
@@ -199,18 +93,6 @@ def __branchExists(repo: str, branch: str) -> bool:
 	"""
 	retCode, _ = runJob(f"git ls-remote --heads {repo}.git {branch} | grep -q refs/heads/{branch}")
 	return not retCode
-
-def __logDoneMessage():
-	"""
-	### This function logs a message shown after completing a task.
-	"""
-	logger(green("Done"), forceConsoleOutput=True, substitute=True)
-
-def __logWorkingMessage():
-	"""
-	### This function logs a message shown as placeholder for small tasks in progress.
-	"""
-	logger(yellow("Working..."), forceConsoleOutput=True)
 
 def __cloneSourceRepo(repo: str, branch: str=None, path: str='') -> Tuple[int, str]:
 	"""
@@ -291,56 +173,3 @@ def __getPredefinedError(realRetCode: int=0, desiredRetCode: int=0) -> int:
 	- desiredRetCode (int): Optional. Predefined code corresponding to caller code.
 	"""
 	return realRetCode if realRetCode == INTERRUPTED_ERROR else desiredRetCode
-
-def __getCurrentCommit(dir: str="./") -> str:
-	"""
-	### This function returns the current commit short hash of a given repository:
-
-	#### Params:
-	- dir (str): Optional. Directory to repository.
-
-	#### Returns:
-	- (str): Current commit short hash, or empty string if it is not a repo or there were errors.
-	"""
-	retCode, output = runJob("git rev-parse --short HEAD", cwd=dir)
-	if retCode or not output:
-		return ''
-	return output
-
-def __getCommitName(commit: str, dir: str="./") -> str:
-	"""
-	### This function returns the name of the commit branch. It can be a branch name or a release name.
-
-	#### Params:
-	- commit (str): Commit hash
-	- dir (str): Optional. Directory to repository.
-
-	#### Returns:
-	- (str): Name of the commit branch or release.
-	"""
-	retCode, output = runJob(f"git name-rev {commit}", cwd=dir)
-	if retCode or not output:
-		return ''
-
-	# Extract name from commit
-	return output.replace(commit, "").replace(" ", "")
-
-def __getVersionLineWithSpaces(text: str, desiredLen: int) -> str:
-	"""
-	### This function returns the given text with additional spaces so that it has the desired length.
-
-	#### Params:
-	- text (str): Text where extra spaces could be added to.
-	- desiredLen (int): Target length for the text.
-
-	#### Returns:
-	- (str): Given text with as much spaces to the right so that it fits the desired length.
-	"""
-	# Check if text is already as long or longer than the desired length
-	textLen = len(text)
-	if textLen >= desiredLen:
-		return text
-	
-	# Crafting spaces and adding them to the right
-	spaces = ''.join([' ' for _ in range(desiredLen - textLen)])
-	return f"{text}{spaces}"
