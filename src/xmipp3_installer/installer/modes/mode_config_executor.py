270094ac76f1f2cc8483ddf2cfdc67b5feb8140e
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-01-14 16:51:42 +0100

    Get working message more specific

--- src/xmipp3_installer/installer/modes/mode_config_executor.py
+++ src/xmipp3_installer/installer/modes/mode_config_executor.py
@@ -1,9 +1,11 @@
+import os
 from typing import Dict, Tuple
 
 from xmipp3_installer.application.logger import errors
 from xmipp3_installer.application.logger import predefined_messages
 from xmipp3_installer.application.logger.logger import logger
 from xmipp3_installer.application.cli.arguments import params
+from xmipp3_installer.installer import constants
 from xmipp3_installer.installer.modes import mode_executor
 from xmipp3_installer.repository import config
 
@@ -21,13 +23,18 @@ class ModeConfigExecutor(mode_executor.ModeExecutor):
 		- (tuple(int, str)): Tuple containing the error status and an error message if there was an error. 
 		"""
 		logger(predefined_messages.get_section_message("Managing config file"))
-		logger(predefined_messages.get_working_message())
+		action_message = (
+			"Generating config file from scratch with default values..."
+			if self.overwrite or not os.path.exists(constants.CONFIG_FILE) else
+			"Reading config file..."
+		)
+		logger(action_message)
 		try:
 			file_handler = config.ConfigurationFileHandler()
 			file_handler.write_config(overwrite=self.overwrite)
 		except PermissionError as permission_error:
 			return errors.IO_ERROR, str(permission_error)
 		self.config_values = file_handler.values
-		logger(predefined_messages.get_done_message(), substitute=True)
+		logger(predefined_messages.get_done_message())
 		return 0, ""
 	
