ae4f6529cdcd6364462cc251103c5953b05ac78f
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-01-14 18:37:03 +0100

    Added success print on exit

--- src/xmipp3_installer/installer/installer_service.py
+++ src/xmipp3_installer/installer/installer_service.py
@@ -2,14 +2,19 @@ import sys
 from typing import Dict
 
 from xmipp3_installer.application.cli.arguments import modes
-from xmipp3_installer.installer.modes import mode_selector
 from xmipp3_installer.application.logger.logger import logger
+from xmipp3_installer.installer.modes.mode_executor import ModeExecutor
+from xmipp3_installer.application.logger import predefined_messages
+from xmipp3_installer.installer.modes import mode_selector
+
 
 def run_installer(args: Dict):
   mode = args.get(modes.MODE, modes.MODE_ALL)
-  mode_executor = mode_selector.MODE_EXECUTORS[mode](args)
+  mode_executor: ModeExecutor = mode_selector.MODE_EXECUTORS[mode](args)
   ret_code, output = mode_executor.run()
   if ret_code:
     logger.log_error(output, ret_code=ret_code)
-  # Else print done?
+  else:
+    if mode_executor.prints_banner_on_exit:
+      logger(predefined_messages.get_success_message())
   sys.exit(ret_code)
