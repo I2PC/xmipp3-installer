4ca0d1e0cd811318ceeda6de37d5cd3fca613b27
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-01-24 10:43:05 +0100

    Includ shared function

--- tests/e2e/mode_add_model_test.py
+++ tests/e2e/mode_add_model_test.py
@@ -12,7 +12,7 @@ from xmipp3_installer.application.cli.arguments import modes
 from xmipp3_installer.installer.modes import mode_add_model_executor
 
 from .shell_command_outputs import mode_add_model
-from .. import copy_file_from_reference, delete_paths, get_assertion_message
+from .. import copy_file_from_reference, delete_paths, get_assertion_message, get_test_file
 
 __SYNC_PROGRAM_ROOT = "dist"
 __MODEL_PATH = f"./tests/e2e/test_files/{mode_add_model.MODEL_NAME}"
@@ -56,13 +56,6 @@ def __get_sync_program_root():
 	root_components = path_components[:root_index + 1]
 	return os.path.join(*root_components)
 
-def __get_executable_file(name):
-	return os.path.join(
-		os.path.dirname(os.path.abspath(__file__)),
-		"test_files",
-		name
-	)
-
 def __add_execution_permission(file_path):
 	current_mode = os.stat(file_path).st_mode
 	new_mode = current_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH
@@ -81,7 +74,7 @@ def __setup_evironment(request, __mock_sync_program_name):
 			delete_paths([sync_program_root])
 		else:
 			copy_file_from_reference(
-				__get_executable_file(__mock_sync_program_name),
+				get_test_file(__mock_sync_program_name),
 				dest_executable_file
 			)
 			__add_execution_permission(dest_executable_file)
