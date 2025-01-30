cc095501e876019d64484d1193ff9d5c1547ef8c
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-01-30 18:09:04 +0100

    E2E tests

---
+++ tests/e2e/mode_clean_all_test.py
@@ -0,0 +1,73 @@
+import os
+import subprocess
+
+import pytest
+
+from xmipp3_installer.application.cli.arguments import modes
+from xmipp3_installer.installer import constants
+from xmipp3_installer.installer.tmp import versions
+from xmipp3_installer.repository import file_operations
+
+from .. import get_assertion_message
+
+__SOURCES_PATHS = [
+  os.path.join(constants.SOURCES_PATH, source)
+  for source in constants.XMIPP_SOURCES
+]
+
+@pytest.mark.parametrize(
+  "confirmation_text",
+  [
+    pytest.param(""),
+    pytest.param("no"),
+    pytest.param("Yes"),
+    pytest.param("YeS")
+  ]
+)
+def test_deletes_expected_files(__setup_environment, confirmation_text):
+  remain = confirmation_text != "YeS"
+  command_words = ["xmipp3_installer", modes.MODE_CLEAN_ALL]
+  subprocess.run(
+    command_words,
+    capture_output=True,
+    text=True,
+    input=confirmation_text
+  )
+  
+  for remaining_path in [
+    *__SOURCES_PATHS,
+    constants.INSTALL_PATH,
+    constants.BUILD_PATH,
+    constants.CONFIG_FILE
+  ]:
+    file_exists = os.path.exists(remaining_path)
+    assert (
+      file_exists == remain
+    ), get_assertion_message(
+      f"{remaining_path} existence",
+      remain,
+      file_exists
+    )
+
+def __create_config_file():
+  with open(constants.CONFIG_FILE, "w") as empty_file:
+    empty_file.write("")
+
+def __generate_sources():
+  for source_path in __SOURCES_PATHS:
+    os.makedirs(source_path, exist_ok=True)
+
+@pytest.fixture
+def __setup_environment():
+  try:
+    __generate_sources()
+    os.makedirs(constants.INSTALL_PATH, exist_ok=True)
+    os.makedirs(constants.BUILD_PATH, exist_ok=True)
+    __create_config_file()
+    yield
+  finally:
+    file_operations.delete_paths([
+      *__SOURCES_PATHS,
+      constants.INSTALL_PATH,
+      constants.BUILD_PATH,
+    ])
