e31f01e78d850001cb16e9605346eed7c19ca2fe
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-01-31 12:05:02 +0100

    Switched mode clean all to implementation of interface mode clean executor

--- src/xmipp3_installer/installer/modes/mode_clean/mode_clean_all_executor.py
+++ src/xmipp3_installer/installer/modes/mode_clean/mode_clean_all_executor.py
@@ -1,46 +1,12 @@
 import os
-from typing import Tuple, List
+from typing import List
 
-from xmipp3_installer.application import user_interactions
-from xmipp3_installer.application.logger import errors
-from xmipp3_installer.application.logger import predefined_messages
 from xmipp3_installer.application.logger.logger import logger
 from xmipp3_installer.installer import constants
-from xmipp3_installer.installer.modes import mode_executor
-from xmipp3_installer.repository import file_operations
+from xmipp3_installer.installer.modes.mode_clean import mode_clean_executor
 
-class ModeCleanAllExecutor(mode_executor.ModeExecutor):
-	def run(self) -> Tuple[int, str]:
-		"""
-		### Deletes the compiled binaries.
-
-		#### Returns:
-		- (tuple(int, str)): Tuple containing the error status and an error message if there was an error. 
-		"""
-		if not ModeCleanAllExecutor.__get_confirmation():
-			return errors.INTERRUPTED_ERROR, ""
-		file_operations.delete_paths(ModeCleanAllExecutor.__get_paths_to_delete())
-		logger(predefined_messages.get_done_message())
-		return 0, ""
-	
-	@staticmethod
-	def __get_confirmation() -> bool:
-		"""
-		### Asks the user for confirmation.
-
-		#### Returns:
-		- (bool): True if the user confirms, False otherwise.
-		"""
-		confirmation_text = "YeS"
-		logger('\n'.join([
-			logger.yellow("WARNING: This will DELETE ALL content from src and build, and also the xmipp.conf file."),
-			logger.yellow("\tNotice that if you have unpushed changes, they will be deleted."),
-			logger.yellow(f"\nIf you are sure you want to do this, type '{confirmation_text}' (case sensitive):")
-		]))
-		return user_interactions.get_user_confirmation(confirmation_text)
-	
-	@staticmethod
-	def __get_paths_to_delete() -> List[str]:
+class ModeCleanAllExecutor(mode_clean_executor.ModeCleanExecutor):
+	def _get_paths_to_delete(self) -> List[str]:
 		"""
 		### Returns a list of all the paths to be deleted.
 
@@ -56,4 +22,26 @@ class ModeCleanAllExecutor(mode_executor.ModeExecutor):
 			constants.BUILD_PATH,
 			constants.CONFIG_FILE
 		]
+	
+	def _get_confirmation_keyword(self) -> str:
+		"""
+		### Returns the keyword needed to be introduced by the user to confirm an operation.
 
+		#### Returns:
+		- (str): Confirmation keyword.
+		"""
+		return "YeS"
+
+	def _get_confirmation_message(self) -> str:
+		"""
+		### Returns message to be printed when asking for user confirmation.
+
+		#### Returns:
+		- (str): Confirmation message.
+		"""
+		return '\n'.join([
+			logger.yellow("WARNING: This will DELETE ALL content from src and build, and also the xmipp.conf file."),
+			logger.yellow("\tNotice that if you have unpushed changes, they will be deleted."),
+			logger.yellow(f"\nIf you are sure you want to do this, type '{self._get_confirmation_keyword()}' (case sensitive):")
+		])
+	
