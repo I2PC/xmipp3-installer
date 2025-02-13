62f72368d71e363f20e45eb3eccc169b9c188d04
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-02-13 20:00:08 +0100

    Enabled mode get models

--- src/xmipp3_installer/installer/modes/mode_selector.py
+++ src/xmipp3_installer/installer/modes/mode_selector.py
@@ -3,7 +3,7 @@ from xmipp3_installer.installer.modes import (
   mode_config_executor, mode_version_executor, mode_git_executor
 )
 from xmipp3_installer.installer.modes.mode_clean import mode_clean_all_executor, mode_clean_bin_executor
-from xmipp3_installer.installer.modes.mode_models import mode_add_model_executor
+from xmipp3_installer.installer.modes.mode_models import mode_add_model_executor, mode_get_models_executor
 
 MODE_EXECUTORS = {
   modes.MODE_ADD_MODEL: mode_add_model_executor.ModeAddModelExecutor,
@@ -13,7 +13,7 @@ MODE_EXECUTORS = {
   modes.MODE_COMPILE_AND_INSTALL: NotImplemented,
   modes.MODE_CONFIG_BUILD: NotImplemented,
   modes.MODE_CONFIG: mode_config_executor.ModeConfigExecutor,
-  modes.MODE_GET_MODELS: NotImplemented,
+  modes.MODE_GET_MODELS: mode_get_models_executor.ModeGetModelsExecutor,
   modes.MODE_GET_SOURCES: NotImplemented,
   modes.MODE_GIT: mode_git_executor.ModeGitExecutor,
   modes.MODE_TEST: NotImplemented,
