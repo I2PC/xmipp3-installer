495e73df6289161b817cbeac297db17664977fdb
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-02-14 19:55:14 +0100

    Add scipion test download url

--- src/xmipp3_installer/installer/urls.py
+++ src/xmipp3_installer/installer/urls.py
@@ -4,3 +4,4 @@ DOCUMENTATION_URL = 'https://i2pc.github.io/docs/'
 CMAKE_INSTALL_DOCS_URL = 'https://i2pc.github.io/docs/Installation/InstallationNotes/index.html#cmake'
 API_URL = 'https://xmipp.i2pc.es/api/attempts/'
 MODELS_URL= "http://campins.cnb.csic.es/deep_cons/"
+SCIPION_TESTS_URL = "http://scipion.cnb.csic.es/downloads/scipion/data/tests"
