3400a3b9ae74d43afe69ba6d7f9f24df004c4897
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-02-15 18:42:07 +0100

    Imports only obtain module

--- tests/e2e/shell_command_outputs/mode_sync/__init__.py
+++ tests/e2e/shell_command_outputs/mode_sync/__init__.py
@@ -3,12 +3,12 @@ import os
 from xmipp3_installer.application.logger.logger import logger
 from xmipp3_installer.installer.modes.mode_sync import mode_sync_executor
 
-from .. import IO_ERROR
+from ... import shell_command_outputs
 
 TEST_FILES_DIR = os.path.join(".", "tests", "e2e", "test_files")
 SYNC_PROGRAM_NAME = f"{mode_sync_executor._SYNC_PROGRAM_NAME}.py"
 __NO_PROGRAM_INITIAL_MESSAGE = f"{os.path.join(mode_sync_executor._SYNC_PROGRAM_PATH, SYNC_PROGRAM_NAME)} does not exist."
 NO_PROGRAM = f"""{logger.red(__NO_PROGRAM_INITIAL_MESSAGE)}
 {logger.red("Xmipp needs to be compiled successfully before running this command!")}
-{IO_ERROR}
+{shell_command_outputs.IO_ERROR}
 """
