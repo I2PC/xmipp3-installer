ca20d5aba6b9158d6075c524331854917e369d7a
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-02-16 11:13:48 +0100

    E2E test outputs

---
+++ tests/e2e/shell_command_outputs/mode_sync/mode_test.py
@@ -0,0 +1,45 @@
+import os
+
+from xmipp3_installer.application.logger.logger import logger
+from xmipp3_installer.installer import constants, urls
+
+from .. import mode_sync
+from ...test_files import xmipp_sync_data, test
+
+REAL_MODELS_DIR = os.path.join(constants.SOURCES_PATH, constants.XMIPP, "models")
+FAKE_SYNC_PROGRAM_FULL_PATH = os.path.join(".", "tests", "e2e", "test_files", mode_sync.SYNC_PROGRAM_NAME)
+
+def get_test_messages_section(test_names: str) -> str:
+  test_names_str = ', '.join(test_names)
+  initial_message =  f"\tTests to run: {test_names_str}"
+  fake_calls = [test.MESSAGE for _ in test_names]
+  return "\n".join([
+    initial_message,
+    *fake_calls
+	])
+
+__DOWNLOAD_SECTION = '\n'.join([
+	logger.blue("Downloading the test files"),
+  xmipp_sync_data.MESSAGE
+])
+
+__UPDATE_SECTION = '\n'.join([
+  logger.blue("Updating the test files"),
+  xmipp_sync_data.MESSAGE
+])
+
+def get_download_message(test_params: str) -> str:
+  test_names = test_params.split(" ")
+  return "\n".join([
+    __DOWNLOAD_SECTION,
+    get_test_messages_section(test_names),
+    ""
+	])
+
+def get_update_message(test_params: str) -> str:
+  test_names = test_params.split(" ")
+  return "\n".join([
+    __UPDATE_SECTION,
+    get_test_messages_section(test_names),
+    ""
+	])
