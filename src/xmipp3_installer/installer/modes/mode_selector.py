47ee43bf27945e94dafa5453f1d6aa18547b895a
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-02-28 23:37:14 +0100

    Mode configBuild executor (missing unitary & E2E tests)

--- src/xmipp3_installer/installer/modes/mode_selector.py
+++ src/xmipp3_installer/installer/modes/mode_selector.py
@@ -6,6 +6,7 @@ from xmipp3_installer.installer.modes.mode_clean import mode_clean_all_executor,
 from xmipp3_installer.installer.modes.mode_sync import (
   mode_add_model_executor, mode_get_models_executor, mode_test_executor
 )
+from xmipp3_installer.installer.modes.mode_cmake import mode_config_build_executor
 
 MODE_EXECUTORS = {
   modes.MODE_ADD_MODEL: mode_add_model_executor.ModeAddModelExecutor,
@@ -13,7 +14,7 @@ MODE_EXECUTORS = {
   modes.MODE_CLEAN_ALL: mode_clean_all_executor.ModeCleanAllExecutor,
   modes.MODE_CLEAN_BIN: mode_clean_bin_executor.ModeCleanBinExecutor,
   modes.MODE_COMPILE_AND_INSTALL: NotImplemented,
-  modes.MODE_CONFIG_BUILD: NotImplemented,
+  modes.MODE_CONFIG_BUILD: mode_config_build_executor.ModeConfigBuildExecutor,
   modes.MODE_CONFIG: mode_config_executor.ModeConfigExecutor,
   modes.MODE_GET_MODELS: mode_get_models_executor.ModeGetModelsExecutor,
   modes.MODE_GET_SOURCES: mode_get_sources_executor.ModeGetSourcesExecutor,
