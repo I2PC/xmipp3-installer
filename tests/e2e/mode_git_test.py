c1da1d33db8fbd679c57a34419619084c41c9f0b
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-02-28 13:41:05 +0100

    Adapted E2E tests

--- tests/e2e/mode_git_test.py
+++ tests/e2e/mode_git_test.py
@@ -14,41 +14,56 @@ from .. import get_assertion_message, create_versions_json_file
 __XMIPP_PATH = paths.get_source_path(constants.XMIPP)
 
 @pytest.mark.parametrize(
-	"__setup_evironment,expected_output_function",
+	"__setup_evironment",
 	[
 		pytest.param(
-			(False, False), mode_git.get_git_command_no_xmipp_no_xmipp_core,
-			id="Without xmipp, without xmippCore"
+			(False, False, False),
+			id="Without xmipp, without xmippCore, without xmippViz"
 		),
 		pytest.param(
-			(False, True), mode_git.get_git_command_no_xmipp_with_xmipp_core,
-			id="Without xmipp, with xmippCore"
+			(False, False, True),
+			id="Without xmipp, without xmippCore, with xmippViz"
 		),
 		pytest.param(
-			(True, False), mode_git.get_git_command_with_xmipp_no_xmipp_core,
-			id="With xmipp, without xmippCore"
+			(False, True, False),
+			id="Without xmipp, with xmippCore, without xmippViz"
 		),
 		pytest.param(
-			(True, True), mode_git.get_git_command_with_xmipp_with_xmipp_core,
-			id="With xmipp, with xmippCore"
-		)
+			(False, True, True),
+			id="Without xmipp, with xmippCore, with xmippViz"
+		),
+		pytest.param(
+			(True, False, False),
+			id="With xmipp, without xmippCore, without xmippViz"
+		),
+		pytest.param(
+			(True, False, True),
+			id="With xmipp, without xmippCore, with xmippViz"
+		),
+		pytest.param(
+			(True, True, False),
+			id="With xmipp, with xmippCore, without xmippViz"
+		),
+		pytest.param(
+			(True, True, True),
+			id="With xmipp, with xmippCore, with xmippViz"
+		),
 	],
 	indirect=["__setup_evironment"]
 )
-def test_returns_returns_expected_git_command_output(
+def test_returns_returns_xpected_git_command_output(
 	__setup_evironment,
-	expected_output_function
 ):
 	command_words = ["xmipp3_installer", modes.MODE_GIT, "branch"]
 	result = subprocess.run(command_words, capture_output=True, text=True)
-	expected_output = expected_output_function()
+	expected_output = mode_git.get_git_command(*__setup_evironment)
 	assert (
 		result.stdout == expected_output
 	), get_assertion_message("git command output", expected_output, result.stdout)
 
-@pytest.fixture(params=[False, False])
+@pytest.fixture(params=[False, False, False])
 def __setup_evironment(request):
-	xmipp_exists, xmipp_core_exists = request.param
+	xmipp_exists, xmipp_core_exists, xmipp_viz_exists = request.param
 	try:
 		create_versions_json_file()
 		if not xmipp_exists:
@@ -61,10 +76,17 @@ def __setup_evironment(request):
 			)
 		else:
 			os.makedirs(paths.get_source_path(constants.XMIPP_CORE), exist_ok=True)
-		yield xmipp_exists, xmipp_core_exists
+		if not xmipp_viz_exists:
+			file_operations.delete_paths(
+				[paths.get_source_path(constants.XMIPP_VIZ)]
+			)
+		else:
+			os.makedirs(paths.get_source_path(constants.XMIPP_VIZ), exist_ok=True)
+		yield xmipp_exists, xmipp_core_exists, xmipp_viz_exists
 	finally:
 		file_operations.delete_paths([
 			__XMIPP_PATH,
 			paths.get_source_path(constants.XMIPP_CORE),
+			paths.get_source_path(constants.XMIPP_VIZ),
 			paths.VERSION_INFO_FILE
 		])
