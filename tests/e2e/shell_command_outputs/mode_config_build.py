21669859ee36ac1dbf5c411a8ff5127b6f0ba0a4
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-03-04 09:52:07 +0100

    Waiting for CMake project fix

--- tests/e2e/shell_command_outputs/mode_config_build.py
+++ tests/e2e/shell_command_outputs/mode_config_build.py
@@ -7,17 +7,20 @@ CMAKE_EXECUTABLE = "cmake"
 __CONFIGURE_ERROR = logger.red("\n".join([
   "Error 4: Error configuring with CMake.",
   "Check the inside file 'compilation.log'.",
-  XMIPP_DOCS,
-  ""
+  XMIPP_DOCS
 ]))
 
-SUCCESS = f"""------------------ Configuring with CMake ------------------
-{CMAKE_EXECUTABLE} -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS=-mtune=native -DXMIPP_USE_MATLAB=True -DCMAKE_CXX_FLAGS=-mtune=native -DXMIPP_USE_CUDA=True -DXMIPP_LINK_TO_SCIPION=True -DBUILD_TESTING=True -DXMIPP_USE_MPI=True -DCMAKE_INSTALL_PREFIX=dist -DCMAKE_SKIP_RPATH=True
--- Configuring incomplete, errors occurred!
-{__CONFIGURE_ERROR}"""
+__COMMON_SECTION = f"""------------------ Configuring with CMake ------------------
+{CMAKE_EXECUTABLE} -S . -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=False -DCMAKE_CXX_FLAGS=-mtune=native -DCMAKE_C_FLAGS=-mtune=native -DCMAKE_INSTALL_PREFIX=dist -DCMAKE_SKIP_RPATH=True -DXMIPP_LINK_TO_SCIPION=False -DXMIPP_USE_CUDA=False -DXMIPP_USE_MATLAB=False -DXMIPP_USE_MPI=False"""
 
-FAILURE = f"""------------------ Configuring with CMake ------------------
-{CMAKE_EXECUTABLE} -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=dist -DCMAKE_CXX_FLAGS=-mtune=native -DXMIPP_USE_MATLAB=True -DXMIPP_USE_CUDA=True -DXMIPP_LINK_TO_SCIPION=True -DBUILD_TESTING=True -DCMAKE_SKIP_RPATH=True -DCMAKE_C_FLAGS=-mtune=native -DXMIPP_USE_MPI=True
+SUCCESS = f"""{__COMMON_SECTION}
 -- Building for: NMake Makefiles
 -- Configuring incomplete, errors occurred!
-{__CONFIGURE_ERROR}"""
+{__CONFIGURE_ERROR}
+"""
+
+FAILURE = f"""{__COMMON_SECTION}
+-- Building for: NMake Makefiles
+-- Configuring incomplete, errors occurred!
+{__CONFIGURE_ERROR}
+"""
