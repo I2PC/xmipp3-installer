5e09995cde12151a0965e739c402f259051a6201
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-03-04 20:01:13 +0100

    Enabled mode compileAndInstall

--- src/xmipp3_installer/installer/modes/mode_selector.py
+++ src/xmipp3_installer/installer/modes/mode_selector.py
@@ -6,14 +6,14 @@ from xmipp3_installer.installer.modes.mode_clean import mode_clean_all_executor,
 from xmipp3_installer.installer.modes.mode_sync import (
   mode_add_model_executor, mode_get_models_executor, mode_test_executor
 )
-from xmipp3_installer.installer.modes.mode_cmake import mode_config_build_executor
+from xmipp3_installer.installer.modes.mode_cmake import mode_config_build_executor, mode_compile_and_install_executor
 
 MODE_EXECUTORS = {
   modes.MODE_ADD_MODEL: mode_add_model_executor.ModeAddModelExecutor,
   modes.MODE_ALL: NotImplemented,
   modes.MODE_CLEAN_ALL: mode_clean_all_executor.ModeCleanAllExecutor,
   modes.MODE_CLEAN_BIN: mode_clean_bin_executor.ModeCleanBinExecutor,
-  modes.MODE_COMPILE_AND_INSTALL: NotImplemented,
+  modes.MODE_COMPILE_AND_INSTALL: mode_compile_and_install_executor.ModeCompileAndInstallExecutor,
   modes.MODE_CONFIG_BUILD: mode_config_build_executor.ModeConfigBuildExecutor,
   modes.MODE_CONFIG: mode_config_executor.ModeConfigExecutor,
   modes.MODE_GET_MODELS: mode_get_models_executor.ModeGetModelsExecutor,
