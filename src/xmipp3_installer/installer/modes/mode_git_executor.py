7aafdeb1a2276ee85d8375335511d9bb70c9e362
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-03-06 19:29:21 +0100

    Used git handler

--- src/xmipp3_installer/installer/modes/mode_git_executor.py
+++ src/xmipp3_installer/installer/modes/mode_git_executor.py
@@ -1,4 +1,3 @@
-import os
 from typing import Dict, Tuple
 
 from xmipp3_installer.application.cli.arguments import params
@@ -6,7 +5,7 @@ from xmipp3_installer.application.logger.logger import logger
 from xmipp3_installer.installer import constants
 from xmipp3_installer.installer.constants import paths
 from xmipp3_installer.installer.modes import mode_executor
-from xmipp3_installer.installer.handlers import shell_handler
+from xmipp3_installer.installer.handlers import git_handler
 
 class ModeGitExecutor(mode_executor.ModeExecutor):
 	def __init__(self, context: Dict):
@@ -18,8 +17,7 @@ class ModeGitExecutor(mode_executor.ModeExecutor):
 		"""
 		super().__init__(context)
 		command_param_list = context.pop(params.PARAM_GIT_COMMAND)
-		command_params = ' '.join(command_param_list)
-		self.command = f"git {command_params}"
+		self.command = ' '.join(command_param_list)
 	
 	def run(self) -> Tuple[int, str]:
 		"""
@@ -31,37 +29,13 @@ class ModeGitExecutor(mode_executor.ModeExecutor):
 		logger(f"Running command '{self.command}' for all xmipp sources...")
 
 		for source in [constants.XMIPP, *constants.XMIPP_SOURCES]:
-			logger("")
-			ret_code, output = self.__execute_git_command_for_source(source)
+			logger("\n" + logger.blue(
+				f"Running command for {source} in path {paths.get_source_path(source)}..."
+			))
+			ret_code, output = git_handler.execute_git_command_for_source(
+				self.command, source
+			)
 			if ret_code:
 				return ret_code, output
 
 		return 0, ""
-
-	def __execute_git_command_for_source(self, source: str) -> Tuple[int, str]:
-		"""
-		### Executes the git command for a specific source.
-
-		#### Params:
-		- source (str): The source repository name.
-
-		#### Returns:
-		- (tuple(int, str)): Tuple containing the return code and output message.
-		"""
-		source_path = os.path.abspath(os.path.join(paths.SOURCES_PATH, source))
-		if not os.path.exists(source_path):
-			logger(logger.yellow(
-				f"WARNING: Source {source} does not exist in path {source_path}. Skipping."
-			))
-			return 0, ""
-
-		logger(logger.blue(
-			f"Running command for {source} in path {source_path}..."
-		))
-		
-		return shell_handler.run_shell_command(
-			self.command,
-			cwd=source_path,
-			show_output=True,
-			show_error=True
-		) 
