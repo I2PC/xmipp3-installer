767787d953d0dbc729a3ce5476b5e96e273875a7
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-03-07 11:06:48 +0100

    Mode all with unitary tests

---
+++ src/xmipp3_installer/installer/modes/mode_all_executor.py
@@ -0,0 +1,55 @@
+from typing import Tuple, Dict
+
+from xmipp3_installer.application.cli.arguments import params
+from xmipp3_installer.installer.modes import mode_executor, mode_get_sources_executor
+from xmipp3_installer.installer.modes.mode_cmake import (
+	mode_config_build_executor, mode_compile_and_install_executor
+)
+
+class ModeAllExecutor(mode_executor.ModeExecutor):
+	def __init__(self, context: Dict):
+		"""
+		### Constructor.
+		
+		#### Params:
+		- context (dict): Dictionary containing the installation context variables.
+		"""
+		self.target_branch = context[params.PARAM_BRANCH]
+		self.jobs = context[params.PARAM_JOBS]
+		self.get_sources_executor = mode_get_sources_executor.ModeGetSourcesExecutor(
+			context
+		)
+		self.config_build_executor = mode_config_build_executor.ModeConfigBuildExecutor(
+			context
+    )
+		self.compile_and_install_executor = mode_compile_and_install_executor.ModeCompileAndInstallExecutor(
+			{**context, params.PARAM_BRANCH: None}
+    )
+		super().__init__(context)
+
+	def _set_executor_config(self):
+		"""
+		### Sets the specific executor params for this mode.
+		"""
+		super()._set_executor_config()
+		self.logs_to_file = True
+		self.prints_with_substitution = True
+		self.prints_banner_on_exit = True
+		self.sends_installation_info = True
+
+	def run(self) -> Tuple[int, str]:
+		"""
+		### Runs the whole installation process with the appropiate params.
+
+		#### Returns:
+		- (tuple(int, str)): Tuple containing the error status and an error message if there was an error. 
+		"""
+		for executor in [
+			self.get_sources_executor,
+			self.config_build_executor,
+			self.compile_and_install_executor
+		]:
+			ret_code, output = executor.run()
+			if ret_code:
+				return ret_code, output
+		return 0, ""
