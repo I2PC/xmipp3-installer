88e5e278441c9dd67d7c27334a48bc23a3bab324
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-03-07 14:42:01 +0100

    Extracted removal function

--- tests/e2e/shell_command_outputs/mode_cmake/mode_config_build.py
+++ tests/e2e/shell_command_outputs/mode_cmake/mode_config_build.py
@@ -8,7 +8,6 @@ from . import (
 EXECUTION_TIME = "X.Y"
 BUILD_FILES_WRITTEN_MESSAGE_START = "-- Build files have been written to: "
 VALID_PATH = get_project_abs_subpath(VALID_PROJECT, "build")
-GENERATOR_LINE = "-- Building for: Ninja\n"
 
 __COMMON_SECTION = f"""------------------ Configuring with CMake ------------------
 {CMAKE_EXECUTABLE} -S . -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=False -DCMAKE_CXX_FLAGS=-mtune=native -DCMAKE_C_FLAGS=-mtune=native -DCMAKE_INSTALL_PREFIX=dist -DCMAKE_SKIP_RPATH=True -DXMIPP_LINK_TO_SCIPION=False -DXMIPP_USE_CUDA=False -DXMIPP_USE_MATLAB=False -DXMIPP_USE_MPI=False"""
@@ -26,3 +25,6 @@ FAILURE = f"""{__COMMON_SECTION}
 
 def normalize_execution_times(raw_output: str) -> str: # Execution times vary from one execution to another
 	return re.sub(r'\(\d+\.\ds\)', f"({EXECUTION_TIME}s)", raw_output)
+
+def remove_generator_line(raw_output: str) -> str: # Generator used is sometimes printed, others not
+	return raw_output.replace("-- Building for: Ninja\n", "")
