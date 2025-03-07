a010df02b76e1f4642ddb18b24750e41cd088346
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-03-07 11:16:17 +0100

    Enabled mode all

--- src/xmipp3_installer/installer/modes/mode_selector.py
+++ src/xmipp3_installer/installer/modes/mode_selector.py
@@ -1,6 +1,7 @@
 from xmipp3_installer.application.cli.arguments import modes
 from xmipp3_installer.installer.modes import (
-  mode_config_executor, mode_version_executor, mode_git_executor, mode_get_sources_executor
+  mode_config_executor, mode_version_executor, mode_git_executor,
+  mode_get_sources_executor, mode_all_executor
 )
 from xmipp3_installer.installer.modes.mode_clean import mode_clean_all_executor, mode_clean_bin_executor
 from xmipp3_installer.installer.modes.mode_sync import (
@@ -10,7 +11,7 @@ from xmipp3_installer.installer.modes.mode_cmake import mode_config_build_execut
 
 MODE_EXECUTORS = {
   modes.MODE_ADD_MODEL: mode_add_model_executor.ModeAddModelExecutor,
-  modes.MODE_ALL: NotImplemented,
+  modes.MODE_ALL: mode_all_executor.ModeAllExecutor,
   modes.MODE_CLEAN_ALL: mode_clean_all_executor.ModeCleanAllExecutor,
   modes.MODE_CLEAN_BIN: mode_clean_bin_executor.ModeCleanBinExecutor,
   modes.MODE_COMPILE_AND_INSTALL: mode_compile_and_install_executor.ModeCompileAndInstallExecutor,
