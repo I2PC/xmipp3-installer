be87275c39c54addb14fa6e5e702234ac791f216
MartinSalinas98 <ssalinasmartin@gmail.com>
2025-03-19 14:55:50 +0100

    Some ruff checks

--- src/xmipp3_installer/shared/singleton.py
+++ src/xmipp3_installer/shared/singleton.py
@@ -3,24 +3,22 @@
 from typing_extensions import Self
 
 class Singleton:
-	"""
-	### Generic singleton class.
-	"""
+  """### Generic singleton class."""
 
-	__instance = None
+  __instance = None
 
-	def __new__(cls, *args, **kwrgs) -> Self:
-		"""
-		### Singleton instancer.
+  def __new__(cls, *args, **kwrgs) -> Self:
+    """
+    ### Singleton instancer.
 
-		#### Params:
-		- cls (self): Current class.
-		- *args (any): Positional params.
-		- **kwargs (any): Argument params.
+    #### Params:
+    - cls (self): Current class.
+    - *args (any): Positional params.
+    - **kwargs (any): Argument params.
 
-		#### Returns:
-		- (self): Instance of current class.
-		"""
-		if not cls.__instance:
-			cls.__instance = super().__new__(cls)
-		return cls.__instance
+    #### Returns:
+    - (self): Instance of current class.
+    """
+    if not cls.__instance:
+      cls.__instance = super().__new__(cls)
+    return cls.__instance
