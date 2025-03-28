907d0051ad85bf515a99e843129166ef02fdca5f
ssalinasmartin@gmail.com <ssalinasmartin@gmail.com>
2025-03-28 13:34:10 +0100

    Change models default dir

--- tests/integration/application/cli/mode_messages/mode_get_models.py
+++ tests/integration/application/cli/mode_messages/mode_get_models.py
@@ -2,18 +2,18 @@ from .. import terminal_sizes
 from . import NOTE_MESSAGE
 
 HELP_MESSAGE = {
-  terminal_sizes.LARGE_TERMINAL_WIDTH: f"""Download the DeepLearning Models at dir/models (build by default).
+  terminal_sizes.LARGE_TERMINAL_WIDTH: f"""Download the DeepLearning Models at dir/models (dist by default).
 
 {NOTE_MESSAGE}Usage: xmipp getModels [options]
     ---------------------------------------------
     # Options #
 
-    -d, --directory                                Directory where models will be saved. Default is "build".
+    -d, --directory                                Directory where models will be saved. Default is "dist".
 
 Example 1: ./xmipp getModels
 Example 2: ./xmipp getModels -d /path/to/my/model/directory
 """,
-  terminal_sizes.SHORT_TERMINAL_WIDTH: f"""Download the DeepLearning Models at dir/models (build by default).
+  terminal_sizes.SHORT_TERMINAL_WIDTH: f"""Download the DeepLearning Models at dir/models (dist by default).
 
 {NOTE_MESSAGE}Usage: xmipp getModels [options]
     ---------------------------------------------
@@ -21,7 +21,7 @@ Example 2: ./xmipp getModels -d /path/to/my/model/directory
 
     -d, --directory                                Directory where models
                                                    will be saved. Default is
-                                                   "build".
+                                                   "dist".
 
 Example 1: ./xmipp getModels
 Example 2: ./xmipp getModels -d /path/to/my/model/directory
