0f18d249a737d2d0d004e71dee73eb10f2f5cdd6
ssalinasmartin@gmail.com <ssalinasmartin@gmail.com>
2025-03-28 12:41:44 +0100

    Fixed models url

--- src/xmipp3_installer/installer/urls.py
+++ src/xmipp3_installer/installer/urls.py
@@ -3,6 +3,6 @@
 DOCUMENTATION_URL = 'https://i2pc.github.io/docs/'
 CMAKE_INSTALL_DOCS_URL = 'https://i2pc.github.io/docs/Installation/Requirements/index.html#cmake'
 API_URL = 'https://xmipp.i2pc.es/api/attempts/'
-MODELS_URL= "http://campins.cnb.csic.es/deep_cons/"
+MODELS_URL= "http://scipion.cnb.csic.es/downloads/scipion/software/em"
 SCIPION_TESTS_URL = "http://scipion.cnb.csic.es/downloads/scipion/data/tests"
 I2PC_REPOSITORY_URL = "https://github.com/i2pc/"
