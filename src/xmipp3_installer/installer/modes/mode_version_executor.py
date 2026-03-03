441b8891b8d1557fdf243320bf7730dca0697088
martin-s-a <ssalinasmartin@gmail.com>
2026-03-03 10:18:13 +0100

    Ruff fixes

--- src/xmipp3_installer/installer/modes/mode_version_executor.py
+++ src/xmipp3_installer/installer/modes/mode_version_executor.py
@@ -3,8 +3,9 @@
 
 This module contains the class to collect and display version information.
 """
+from __future__ import annotations
+
 import os
-from typing import Dict, Tuple
 
 from xmipp3_installer.api_client.assembler import installation_info_assembler
 from xmipp3_installer.application.cli.arguments import params
@@ -26,7 +27,7 @@ class ModeVersionExecutor(mode_executor.ModeExecutor):
   
   __LEFT_TEXT_LEN = 25
 
-  def __init__(self, context: Dict):
+  def __init__(self, context: dict):
     """
     ### Constructor.
     
@@ -43,7 +44,7 @@ class ModeVersionExecutor(mode_executor.ModeExecutor):
     self.xmipp_version_name = versions.xmipp_version_name
     self.release_date = versions.xmipp_release_date
   
-  def run(self) -> Tuple[int, str]:
+  def run(self) -> tuple[int, str]:
     """
     ### Collects all the version information available and displays it.
 
@@ -95,10 +96,10 @@ class ModeVersionExecutor(mode_executor.ModeExecutor):
     #### Returns:
     - (str): Sources related message section.
     """
-    sources_message_lines = []
-    for source_package in constants.XMIPP_SOURCES:
-      sources_message_lines.append(self.__get_source_info(source_package))
-    return '\n'.join(sources_message_lines)
+    return '\n'.join([
+      self.__get_source_info(source_package)
+      for source_package in constants.XMIPP_SOURCES
+    ])
 
   def __get_source_info(self, source: str) -> str:
     """
